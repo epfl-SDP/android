3559575f02c94fdb8172cc4d956b526a68775fc7
matt989253 <matthieu.burguburu@gmail.com>
2022-03-04 15:38:40 +0100

    Add Firebase dependencies to Gradle

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
   id("com.ncorti.ktfmt.gradle")
   id("org.jetbrains.kotlin.android")
   id("jacoco")
+  id("com.google.gms.google-services")
 }
 
 android {
@@ -67,6 +68,11 @@ dependencies {
   androidTestImplementation(libs.compose.ui.test.junit4)
   debugImplementation(libs.compose.ui.test.manifest)
   debugImplementation(libs.compose.ui.tooling.tooling)
+
+  // Firebase
+  implementation (platform (libs.firebase.bom))
+  implementation("com.google.firebase:firebase-auth-ktx")
+  implementation("com.google.firebase:firebase-firestore-ktx")
 
   // Kotlin and coroutines.
   implementation(libs.bundles.coroutines.android)
