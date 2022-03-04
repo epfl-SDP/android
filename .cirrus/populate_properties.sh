e715a233b2ab6e1a40af2b9e453552d1525452d6
matt989253 <matthieu.burguburu@gmail.com>
2022-03-04 15:44:07 +0100

    Add Google Services to Cirrus CI
    Co-authored-by: Alexandre Piveteau <alexandre@piveteau.email>

--- .cirrus/populate_properties.sh
+++ .cirrus/populate_properties.sh
@@ -3,3 +3,6 @@
 # Takes $GITHUB_USERNAME and $GITHUB_TOKEN and writes them to the ~/.gradle/gradle.properties
 mkdir ~/.gradle
 echo "githubJacocoUsername=$GITHUB_USERNAME\ngithubJacocoPassword=$GITHUB_TOKEN" > ~/.gradle/gradle.properties
+
+# Takes $GOOGLE_SERVICES and writes it to ./mobile/google-services.json
+echo $GOOGLE_SERVICES > .mobile/google-services.json
