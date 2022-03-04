eecc62f7705878632f650765b26c3f9e3477b0f7
Alexandre Piveteau <alexandre@piveteau.email>
2022-03-04 13:47:10 +0100

    Add the view-models for the `Profile` and `Following` screens

---
+++ mobile/src/main/java/ch/epfl/sdp/mobile/ui/features/ProfileColor.kt
@@ -0,0 +1,9 @@
+package ch.epfl.sdp.mobile.ui.features
+
+/**
+ * An enumeration representing the different colors which can be used as the background of a profile
+ * picture.
+ */
+enum class ProfileColor {
+  Pink,
+}
