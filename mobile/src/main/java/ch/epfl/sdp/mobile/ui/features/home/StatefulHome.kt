1e8a6632de67030e71c978727e0b4752b56a2692
Alexandre Piveteau <alexandre@piveteau.email>
2022-03-05 11:42:56 +0100

    Document `StatefulHome`

--- mobile/src/main/java/ch/epfl/sdp/mobile/ui/features/home/StatefulHome.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/ui/features/home/StatefulHome.kt
@@ -14,9 +14,20 @@ import ch.epfl.sdp.mobile.data.api.AuthenticationApi
 import ch.epfl.sdp.mobile.ui.features.profile.StatefulProfileScreen
 import ch.epfl.sdp.mobile.ui.features.social.StatefulFollowingScreen
 
+/** The route associated to the social tab. */
 private const val SocialRoute = "social"
+
+/** The route associated to the settings tab. */
 private const val SettingsRoute = "settings"
 
+/**
+ * A stateful composable, which is used at the root of the navigation when the user is
+ * authenticated. It displays the bottom navigation sections.
+ *
+ * @param user the currently logged-in user.
+ * @param modifier the [Modifier] for this composable.
+ * @param controller the [NavHostController] used to control the current destination.
+ */
 @Composable
 fun StatefulHome(
     user: AuthenticationApi.User,
@@ -40,6 +51,7 @@ fun StatefulHome(
   }
 }
 
+/** Maps a [NavBackStackEntry] to the appropriate [HomeSection]. */
 private fun NavBackStackEntry.toSection(): HomeSection? =
     when (destination.route) {
       SocialRoute -> HomeSection.Social
@@ -47,6 +59,7 @@ private fun NavBackStackEntry.toSection(): HomeSection? =
       else -> null
     }
 
+/** Maps a [HomeSection] to the appropriate route. */
 private fun HomeSection.toRoute(): String =
     when (this) {
       HomeSection.Social -> SocialRoute
