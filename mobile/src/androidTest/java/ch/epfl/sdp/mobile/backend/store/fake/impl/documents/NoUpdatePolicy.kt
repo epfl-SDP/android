963ca7c0bf2404c7c8985c61abac53e48bc9d378
Alexandre Piveteau <alexandre@piveteau.email>
2022-03-08 17:43:06 +0100

    Update Firestore mocking DSL to support only data classes as documents

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/backend/store/fake/impl/documents/NoUpdatePolicy.kt
+++
@@ -1,8 +0,0 @@
-package ch.epfl.sdp.mobile.backend.store.fake.impl.documents
-
-import ch.epfl.sdp.mobile.backend.store.fake.UpdatePolicy
-
-class NoUpdatePolicy(value: Any?) : UpdatePolicy<Any?> {
-  override val empty: Any? = value
-  override fun Any?.update(fields: Map<String, Any?>): Any? = this
-}
