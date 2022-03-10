c10d25ff612e8995c56b418c8a3cbd8b2280cf63
Lars Barmettler <barmettler.lars@gmail.com>
2022-03-10 13:13:25 +0100

    Connect Social screen with state (#40)

--- mobile/src/main/java/ch/epfl/sdp/mobile/ui/features/social/StatefulFollowingScreen.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/ui/features/social/StatefulFollowingScreen.kt
@@ -1,23 +1,41 @@
 package ch.epfl.sdp.mobile.ui.features.social
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.tooling.preview.Preview
 import ch.epfl.sdp.mobile.data.api.AuthenticationApi
+import ch.epfl.sdp.mobile.data.api.ProfileColor
 
-@Preview
+
+private class SnapshotFollowingState(
+    private val following: State<List<AuthenticationApi.Profile>>,
+) : FollowingState {
+
+    private data class ProfileAdapter(
+        private val profile: AuthenticationApi.Profile,
+    ) : Person {
+        override val backgroundColor: ProfileColor
+            get() = profile.backgroundColor
+        override val name: String
+            get() = profile.name
+        override val emoji: String
+            get() = profile.emoji
+    }
+
+    override val players: List<Person>
+        get() = following.value.map { ProfileAdapter(it) }
+}
+
+
 @Composable
 fun StatefulFollowingScreen(
     user: AuthenticationApi.User.Authenticated,
     modifier: Modifier = Modifier,
 ) {
-  val following by user.following.collectAsState(initial = emptyList())
+    val state = remember(user) { user.following }.collectAsState(emptyList())
 
-  Box(modifier, Alignment.Center) { following.forEach { profile ->  Text(profile.name, modifier = Modifier.testTag("following")) } }
+    SocialScreen(SnapshotFollowingState(state), modifier.fillMaxSize())
 }
