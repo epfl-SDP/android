03fc9bcf49bdf28e096341093fac69caa1e63d42
Lars Barmettler <barmettler.lars@gmail.com>
2022-03-10 14:00:53 +0100

    reformat code (#40)

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/ui/features/social/StatefulFollowingScreenTest.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/ui/features/social/StatefulFollowingScreenTest.kt
@@ -4,39 +4,32 @@ import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import ch.epfl.sdp.mobile.data.api.AuthenticationApi
 import ch.epfl.sdp.mobile.data.api.ProfileColor
-import ch.epfl.sdp.mobile.ui.features.home.StatefulHome
-import ch.epfl.sdp.mobile.ui.i18n.setContentWithLocalizedStrings
 import io.mockk.every
 import io.mockk.mockk
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flow
 import org.junit.Rule
 import org.junit.Test
 
-
 class StatefulFollowingScreenTest {
-    @get:Rule
-    val rule = createComposeRule()
+  @get:Rule val rule = createComposeRule()
 
-
-    @Test
-    fun defaultMode_displayCorrectFollowers() {
-        val mockUser = mockk<AuthenticationApi.User.Authenticated>()
-        every { mockUser.following } returns flow {
-            emit(listOf<AuthenticationApi.Profile>(
-                object : AuthenticationApi.Profile {
+  @Test
+  fun defaultMode_displayCorrectFollowers() {
+    val mockUser = mockk<AuthenticationApi.User.Authenticated>()
+    every { mockUser.following } returns
+        flow {
+          emit(
+              listOf<AuthenticationApi.Profile>(
+                  object : AuthenticationApi.Profile {
                     override val emoji: String
-                        get() = ":>"
+                      get() = ":>"
                     override val name: String
-                        get() = "Hans Peter"
+                      get() = "Hans Peter"
                     override val backgroundColor: ProfileColor
-                        get() = ProfileColor.Pink
-                }
-            )
-            )
+                      get() = ProfileColor.Pink
+                  }))
         }
-        rule.setContent { StatefulFollowingScreen(mockUser) }
-        rule.onNodeWithText("Hans Peter").assertExists()
-    }
+    rule.setContent { StatefulFollowingScreen(mockUser) }
+    rule.onNodeWithText("Hans Peter").assertExists()
+  }
 }
