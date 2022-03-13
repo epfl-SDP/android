5b617554fceccfb367bf456d09ce95d981a46ebc
Alexandre Piveteau <alexandre@piveteau.email>
2022-03-13 22:40:10 +0100

    Add `Game.play { ... }` DSL for tests

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/application/chess/GameTest.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/application/chess/GameTest.kt
@@ -15,10 +15,19 @@ class GameTest {
 
   @Test
   fun emptyGame_validMove_switchesNextPlayer() {
-    var game = emptyGame()
-    val step1 = game.nextStep as NextStep.MovePiece
-    game = step1.move(Position(0, 6), Delta(0, 1))
-    val step2 = game.nextStep as NextStep.MovePiece
-    assertThat(step2.turn).isEqualTo(Color.Black)
+    val game = emptyGame().play { Position(0, 6) += Delta(0, 1) }
+    val step = game.nextStep as NextStep.MovePiece
+    assertThat(step.turn).isEqualTo(Color.Black)
+  }
+
+  @Test
+  fun emptyGame_twoValidMoves_switchesFirstPlayer() {
+    val game =
+        emptyGame().play {
+          Position(0, 6) += Delta(0, -1)
+          Position(0, 1) += Delta(0, 1)
+        }
+    val step = game.nextStep as NextStep.MovePiece
+    assertThat(step.turn).isEqualTo(Color.White)
   }
 }
