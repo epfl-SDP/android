fc9412e623159e94372213adc2123fc4cc5e9522
Alexandre Piveteau <alexandre@piveteau.email>
2022-03-14 16:37:10 +0100

    Implement a basic immutable chess engine
    * Add `Position` and `Delta` classes
    
    * Add `Game`, `NextStep` and types related to `Board`
    
    * Add a dependency on `kotlinx.collections.immutable`
    
    * Add `emptyBoard() builder for chess games`
    
    * Add some tests to `Position`
    
    * Add `PersistentBoard` and use it in `PersistentGame`
    
    * Fix `Position.inBounds`
    
    * Add `Game.play { ... }` DSL for tests
    
    * Use a different syntax for `for` loops to somehow increase code coverage
    
    * Set the initial board pieces up in `emptyGame()`

---
+++ mobile/src/main/java/ch/epfl/sdp/mobile/application/chess/Color.kt
@@ -0,0 +1,11 @@
+package ch.epfl.sdp.mobile.application.chess
+
+/** The classical colors of a game of chess. */
+enum class Color {
+
+  /** The black color. */
+  Black,
+
+  /** The white color. Starts the game. */
+  White,
+}
