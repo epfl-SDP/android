21838c28a36d6988dfb7457ee7ebe1d93a7776b4
Alexandre Piveteau <alexandre@piveteau.email>
2022-03-14 10:47:49 +0100

    Order ranks by their point values

--- mobile/src/main/java/ch/epfl/sdp/mobile/application/chess/Rank.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/application/chess/Rank.kt
@@ -2,10 +2,10 @@ package ch.epfl.sdp.mobile.application.chess
 
 /** An enumeration representing the abilities of each [Piece] in classic chess. */
 enum class Rank {
-  Bishop,
   King,
-  Knight,
-  Pawn,
   Queen,
   Rook,
+  Bishop,
+  Knight,
+  Pawn,
 }
