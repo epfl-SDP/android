1af00c0fc38ccb87db798c6e77c755ab7376a35f
matt989253 <matthieu.burguburu@gmail.com>
2022-03-14 19:39:27 +0100

    Add chessboard without pieces

--- mobile/src/main/java/ch/epfl/sdp/mobile/ui/game/GameScreen.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/ui/game/GameScreen.kt
@@ -1,10 +1,9 @@
 package ch.epfl.sdp.mobile.ui.game
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -12,6 +11,9 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -21,9 +23,10 @@ fun GameScreen(
     modifier: Modifier = Modifier,
 ) {
   Column(modifier) {
-    TempText(text = "Chess game")
+    TitleText(text = "Chess game")
     MoveList(state.moves, modifier)
-    TempText(text = "Voice Recognition")
+    Chessboard(size = 8)
+    TitleText(text = "Voice Recognition")
   }
 }
 
@@ -51,7 +54,34 @@ fun MoveList(moves: List<Move>, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun TempText(text: String, modifier: Modifier = Modifier) {
+fun Chessboard(modifier: Modifier = Modifier, size: Int = 8) {
+  BoxWithConstraints(modifier.fillMaxSize().aspectRatio(1f).padding(16.dp)) {
+    Column(modifier = Modifier.squareBorder(2.dp)) {
+      for (i in 0 until size) {
+        Row(modifier = Modifier.weight(1f)) {
+          for (j in 0 until size) {
+            Square((i + j) % 2 == 1, modifier = Modifier.weight(1f))
+          }
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun Square(darkSquare: Boolean, modifier: Modifier = Modifier) {
+  val color =
+      if (darkSquare) MaterialTheme.colors.onPrimary.copy(alpha = 0.4f) else Color.Transparent
+
+  Canvas(modifier = modifier.fillMaxSize().squareBorder(1.dp)) { drawRect(color = color) }
+}
+
+fun Modifier.squareBorder(width: Dp): Modifier = composed {
+  border(width = width, color = MaterialTheme.colors.primary, shape = RoundedCornerShape(0.dp))
+}
+
+@Composable
+fun TitleText(text: String, modifier: Modifier = Modifier) {
   Text(
       text = text,
       color = MaterialTheme.colors.primary,
