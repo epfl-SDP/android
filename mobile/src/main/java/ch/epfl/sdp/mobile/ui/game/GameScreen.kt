aabb51d50365d0b57be781c156cf2715ca5fa3d2
matt989253 <matthieu.burguburu@gmail.com>
2022-03-14 22:44:36 +0100

    Change chessboard drawing to be in a Modifier
    Co-authored-by: Alexandre Piveteau <alexandre@piveteau.email>

--- mobile/src/main/java/ch/epfl/sdp/mobile/ui/game/GameScreen.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/ui/game/GameScreen.kt
@@ -1,7 +1,5 @@
 package ch.epfl.sdp.mobile.ui.game
 
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
@@ -11,12 +9,8 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun GameScreen(
     state: GameScreenState,
@@ -25,7 +19,7 @@ fun GameScreen(
   Column(modifier) {
     TitleText(text = "Chess game")
     MoveList(state.moves)
-    Chessboard(size = 8)
+    ChessBoard(cells = 8)
     TitleText(text = "VR / Voice Recognition")
   }
 }
@@ -54,33 +48,6 @@ fun MoveList(moves: List<Move>, modifier: Modifier = Modifier) {
           modifier = Modifier.padding(horizontal = 8.dp, vertical = 8.dp))
     }
   }
-}
-
-@Composable
-fun Chessboard(modifier: Modifier = Modifier, size: Int = 8) {
-  BoxWithConstraints(modifier.aspectRatio(1f).padding(16.dp)) {
-    Column(modifier = Modifier.squareBorder(2.dp)) {
-      for (i in 0 until size) {
-        Row(modifier = Modifier.weight(1f).squareBorder(1.dp)) {
-          for (j in 0 until size) {
-            Square((i + j) % 2 == 1, modifier = Modifier.weight(1f).squareBorder(1.dp))
-          }
-        }
-      }
-    }
-  }
-}
-
-@Composable
-fun Square(darkSquare: Boolean, modifier: Modifier = Modifier) {
-  val color =
-      if (darkSquare) MaterialTheme.colors.onPrimary.copy(alpha = 0.4f) else Color.Transparent
-
-  Canvas(modifier = modifier.fillMaxSize()) { drawRect(color = color) }
-}
-
-fun Modifier.squareBorder(width: Dp): Modifier = composed {
-  border(width = width, color = MaterialTheme.colors.primary, shape = RoundedCornerShape(0.dp))
 }
 
 @Composable
