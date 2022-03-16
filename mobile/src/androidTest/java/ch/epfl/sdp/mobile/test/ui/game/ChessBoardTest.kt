4f601217640a527ee7657215b52c006a640819b1
Alexandre Piveteau <alexandre@piveteau.email>
2022-03-16 10:55:05 +0100

    Add some more tests for `ChessBoard`

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/ui/game/ChessBoardTest.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/ui/game/ChessBoardTest.kt
@@ -1,19 +1,25 @@
 package ch.epfl.sdp.mobile.test.ui.game
 
 import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onAllNodesWithContentDescription
-import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.performTouchInput
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import ch.epfl.sdp.mobile.state.FakeChessBoardState
 import ch.epfl.sdp.mobile.test.ui.contains
 import ch.epfl.sdp.mobile.test.ui.getBoundsInRoot
 import ch.epfl.sdp.mobile.ui.game.ChessBoard
+import ch.epfl.sdp.mobile.ui.game.ChessBoardState
+import ch.epfl.sdp.mobile.ui.game.ChessBoardState.Color.White
+import ch.epfl.sdp.mobile.ui.game.ChessBoardState.Piece
+import ch.epfl.sdp.mobile.ui.game.ChessBoardState.Position
+import ch.epfl.sdp.mobile.ui.game.ChessBoardState.Rank.Pawn
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.test.runTest
 import org.junit.Rule
@@ -38,5 +44,61 @@ class ChessBoardTest {
           DpOffset(90.dp, 90.dp) in it.getBoundsInRoot()
         }
     assertThat(inBounds).isTrue()
+  }
+
+  /**
+   * An implementation of [ChessBoardState] which moves a single piece around the chessboard on drag
+   * and drops.
+   *
+   * @param piece the [Piece] which should be moved.
+   */
+  class SinglePieceSnapshotChessBoardState(
+      private val piece: Piece<Unit> = Piece(Unit, Pawn, White),
+  ) : ChessBoardState<Unit> {
+
+    var position: Position by mutableStateOf(Position(0, 0))
+
+    override val pieces: Map<Position, Piece<Unit>>
+      get() = mapOf(position to piece)
+
+    override val dragEnabled = true
+
+    override fun onDropPiece(
+        piece: Piece<Unit>,
+        startPosition: Position,
+        endPosition: Position,
+    ) {
+      position = endPosition
+    }
+  }
+
+  @Test
+  fun draggingPawnAround_whileBoardIsSuccessful_dropsOnRightTarget() = runTest {
+    val state = SinglePieceSnapshotChessBoardState()
+    rule.setContent { ChessBoard(state, Modifier.size(160.dp).testTag("board")) }
+    rule.onNodeWithTag("board").performTouchInput {
+      down(Offset(10.dp.toPx(), 10.dp.toPx()))
+      moveBy(Offset(0.dp.toPx(), 20.dp.toPx()))
+    }
+    state.position = Position(1, 1)
+    rule.onNodeWithTag("board").performTouchInput {
+      moveBy(Offset(0.dp.toPx(), 20.dp.toPx())) // Still drop at (0, 3)
+      up()
+    }
+    val bounds = rule.onNodeWithContentDescription("White Pawn").getBoundsInRoot()
+    assertThat(DpOffset(10.dp, 50.dp) in bounds).isTrue()
+  }
+
+  @Test
+  fun emptyDragGesture_doesNotMovePawn() = runTest {
+    val state = SinglePieceSnapshotChessBoardState()
+    rule.setContent { ChessBoard(state, Modifier.size(160.dp).testTag("board")) }
+    rule.onNodeWithTag("board").performTouchInput {
+      down(Offset(10.dp.toPx(), 10.dp.toPx()))
+      cancel() // Cancel the gesture, so we should not drop the pawn.
+    }
+    rule.awaitIdle()
+    val bounds = rule.onNodeWithContentDescription("White Pawn").getBoundsInRoot()
+    assertThat(DpOffset(10.dp, 10.dp) in bounds).isTrue()
   }
 }
