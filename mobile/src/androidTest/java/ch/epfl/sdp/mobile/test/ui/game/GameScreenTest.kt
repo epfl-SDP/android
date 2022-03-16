f86ae83475c73137575609fd6f25e8d4c62300e2
matt989253 <matthieu.burguburu@gmail.com>
2022-03-16 19:00:56 +0100

    Add GameScreen tests

---
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/ui/game/GameScreenTest.kt
@@ -0,0 +1,51 @@
+package ch.epfl.sdp.mobile.test.ui.game
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import ch.epfl.sdp.mobile.state.ChessMove
+import ch.epfl.sdp.mobile.test.state.setContentWithLocalizedStrings
+import ch.epfl.sdp.mobile.ui.game.ChessBoardState
+import ch.epfl.sdp.mobile.ui.game.GameScreen
+import ch.epfl.sdp.mobile.ui.game.GameScreenState
+import ch.epfl.sdp.mobile.ui.game.Move
+import org.junit.Rule
+import org.junit.Test
+
+class GameScreenTest {
+  @get:Rule val rule = createComposeRule()
+
+  private class SnapshotGameScreenState :
+      GameScreenState<Unit>,
+      ChessBoardState<Unit> by ChessBoardTest.SinglePieceSnapshotChessBoardState() {
+    override val moves: List<Move>
+      get() =
+          listOf(
+              ChessMove(1, "f3"),
+              ChessMove(2, "e5"),
+              ChessMove(3, "g4"),
+              ChessMove(4, "Qh4#"),
+          )
+  }
+
+  @Test
+  fun moveList_isDisplayed() {
+    rule.setContent { GameScreen(state = SnapshotGameScreenState()) }
+
+    rule.onNodeWithText("1. f3").assertExists()
+    rule.onNodeWithText("2. e5").assertExists()
+    rule.onNodeWithText("3. g4").assertExists()
+    rule.onNodeWithText("4. Qh4#").assertExists()
+  }
+
+  @Test
+  fun aWhitePawn_isDisplayed() {
+    val strings =
+        rule.setContentWithLocalizedStrings { GameScreen(state = SnapshotGameScreenState()) }
+
+    rule.onNodeWithContentDescription(
+            strings.boardPieceContentDescription(strings.boardColorWhite, strings.boardPiecePawn),
+        )
+        .assertExists()
+  }
+}
