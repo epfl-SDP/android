4e0c8405aa3f787af174b3ff5258104b07b1dd8f
Alexandre Piveteau <alexandre@piveteau.email>
2022-03-17 00:16:57 +0100

    Fix documents not being emitted to existing query collectors
    * Ensure that `FakeStore.collection` returns an empty query snapshot
    
    * Fix documents not being emitted to existing query collectors

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/Coroutines.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/Coroutines.kt
@@ -1,6 +1,7 @@
 package ch.epfl.sdp.mobile.test
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.suspendCancellableCoroutine
@@ -23,4 +24,25 @@ suspend inline fun suspendForever(): Nothing = suspendCancellableCoroutine {}
 inline fun <reified T> Collection<Flow<T>>.combineOrEmpty(): Flow<List<T>> {
   if (isEmpty()) return flowOf(emptyList())
   return combine(this) { it.toList() }
+}
+
+/**
+ * Atomically updates the value of the [MutableStateFlow] and returns a computed value from the
+ * [MutableStateFlow] state. This is a variation of the standard existing `updateAndGet` from
+ * mutable state flows.
+ *
+ * @param T the type of the value of the [MutableStateFlow].
+ * @param R the type of the return value.
+ * @param function a function which maps the [MutableStateFlow] to its next state and the computed
+ * value.
+ * @return the computed value.
+ */
+inline fun <T, R> MutableStateFlow<T>.updateAndGetWithValue(function: (T) -> Pair<T, R>): R {
+  while (true) {
+    val prevValue = value
+    val (nextValue, returnValue) = function(prevValue)
+    if (compareAndSet(prevValue, nextValue)) {
+      return returnValue
+    }
+  }
 }
