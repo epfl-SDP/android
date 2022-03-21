a29f9fa162835ecec6570dffa6ece8a78deaa11a
Alexandre Piveteau <alexandre@piveteau.email>
2022-03-21 12:49:54 +0100

    Add support for `@DocumentId` to `toObject` and `fromObject`

---
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/infrastructure/persistence/store/fake/FakeDocumentId.kt
@@ -0,0 +1,16 @@
+package ch.epfl.sdp.mobile.test.infrastructure.persistence.store.fake
+
+/**
+ * A class representing the unique identifier of a [FakeDocumentRecord]. Within a collection, all
+ * documents are guaranteed to have different ids.
+ *
+ * @param value the value of the unique identifier.
+ */
+data class FakeDocumentId(val value: String) {
+
+  companion object {
+
+    /** The [FakeDocumentId] given to the root of the store. */
+    val Root = FakeDocumentId("")
+  }
+}
