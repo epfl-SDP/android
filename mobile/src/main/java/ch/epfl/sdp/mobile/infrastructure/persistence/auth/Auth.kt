6896b3ce30c4d56b9cdb8aa517739061392eb172
Fouad Mahmoud <fouad.mahmoud@epfl.ch>
2022-03-22 16:01:49 +0100

    clean PR

--- mobile/src/main/java/ch/epfl/sdp/mobile/infrastructure/persistence/auth/Auth.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/infrastructure/persistence/auth/Auth.kt
@@ -3,7 +3,7 @@ package ch.epfl.sdp.mobile.infrastructure.persistence.auth
 import kotlinx.coroutines.flow.Flow
 
 /**
- * An interface representing the primitives that the application may use to perform some basic
+ * An interface representing the primitives that the application may used to perform some basic
  * authentication duties.
  */
 interface Auth {
