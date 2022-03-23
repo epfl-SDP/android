72b619a38f911b8a017293d2fe9a59fec524bd43
Fouad Mahmoud <fouad.mahmoud@epfl.ch>
2022-03-23 15:19:45 +0100

    move statefulFollowingScreen test

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/ui/social/SocialScreenTest.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/ui/social/SocialScreenTest.kt
@@ -7,16 +7,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import ch.epfl.sdp.mobile.application.Profile.Color
-import ch.epfl.sdp.mobile.application.ProfileDocument
-import ch.epfl.sdp.mobile.application.authentication.AuthenticatedUser
-import ch.epfl.sdp.mobile.application.authentication.AuthenticationFacade
-import ch.epfl.sdp.mobile.application.social.SocialFacade
-import ch.epfl.sdp.mobile.infrastructure.persistence.store.asFlow
-import ch.epfl.sdp.mobile.state.ProvideFacades
-import ch.epfl.sdp.mobile.state.StatefulFollowingScreen
-import ch.epfl.sdp.mobile.test.infrastructure.persistence.auth.emptyAuth
-import ch.epfl.sdp.mobile.test.infrastructure.persistence.store.buildStore
-import ch.epfl.sdp.mobile.test.infrastructure.persistence.store.document
 import ch.epfl.sdp.mobile.test.state.setContentWithLocalizedStrings
 import ch.epfl.sdp.mobile.ui.i18n.English.socialFollow
 import ch.epfl.sdp.mobile.ui.i18n.English.socialSearchBarPlaceHolder
@@ -28,10 +18,6 @@ import ch.epfl.sdp.mobile.ui.social.SocialScreenState
 import ch.epfl.sdp.mobile.ui.social.SocialScreenState.Mode.Following
 import ch.epfl.sdp.mobile.ui.social.SocialScreenState.Mode.Searching
 import com.google.common.truth.Truth.*
-import kotlinx.coroutines.flow.filterIsInstance
-import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.test.runTest
 import org.junit.Rule
 import org.junit.Test
 
@@ -160,35 +146,5 @@ class SocialScreenTest {
         }
 
     rule.onAllNodesWithText(strings.socialFollow.uppercase()).onFirst().assertExists()
-  }
-
-  @Test
-  fun searchList_onFollowClickUserIsFollowed() {
-    runTest {
-      val name = "Fred"
-      val auth = emptyAuth()
-      val store = buildStore {
-        collection("users") { document("other", ProfileDocument(name = name)) }
-      }
-      val authenticationFacade = AuthenticationFacade(auth, store)
-      val socialFacade = SocialFacade(auth, store)
-
-      authenticationFacade.signUpWithEmail("example", "name", "password")
-      val user = authenticationFacade.currentUser.filterIsInstance<AuthenticatedUser>().first()
-      val strings =
-          rule.setContentWithLocalizedStrings {
-            ProvideFacades(authenticationFacade, socialFacade) { StatefulFollowingScreen(user) }
-          }
-      rule.onNodeWithText(strings.socialSearchBarPlaceHolder).performTextInput(name)
-      rule.onNodeWithText(strings.socialFollow).performClick()
-      val profile =
-          store
-              .collection("users")
-              .document("other")
-              .asFlow<ProfileDocument>()
-              .filterNotNull()
-              .first()
-      assertThat(profile.followers).contains(user.uid)
-    }
   }
 }
