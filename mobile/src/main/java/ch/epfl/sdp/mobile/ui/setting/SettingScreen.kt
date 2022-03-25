935c90e39f49ffe54717de8062c0f6cd8cc07e41
theMaimu <barmettler.lars@gmail.com>
2022-03-25 10:01:40 +0100

    Update mobile/src/main/java/ch/epfl/sdp/mobile/ui/setting/SettingScreen.kt
    Co-authored-by: Fouad-sys <61212919+Fouad-sys@users.noreply.github.com>

--- mobile/src/main/java/ch/epfl/sdp/mobile/ui/setting/SettingScreen.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/ui/setting/SettingScreen.kt
@@ -57,7 +57,8 @@ fun SettingsScreen(
 }
 
 /**
- * Composes the settings header given the profile [state]. Displays also the ProfilePicture,
+ * Composes the settings header given the profile [SettingScreenState]. Displays also the ProfilePicture,
+
  * SettingsButton, name and email of th user profile
  * @param state state of profile screen
  * @param modifier the [Modifier] for this composable.
