6dee406ac0e748d089b1d0205cc012c6a848b4dc
Alexandre Piveteau <alexandre@piveteau.email>
2022-03-29 20:32:41 +0200

    Use `Intents.intended` to test the AR screen

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/ui/ar/ArScreenKtTest.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/ui/ar/ArScreenKtTest.kt
@@ -1,22 +1,45 @@
 package ch.epfl.sdp.mobile.test.ui.ar
 
-import androidx.compose.ui.semantics.SemanticsProperties
-import androidx.compose.ui.semantics.SemanticsPropertyKey
+import android.Manifest
+import android.app.Activity
+import android.app.Instrumentation.ActivityResult
 import androidx.compose.ui.test.*
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.espresso.intent.Intents.*
+import androidx.test.espresso.intent.matcher.IntentMatchers.anyIntent
+import androidx.test.rule.GrantPermissionRule
+import androidx.test.rule.GrantPermissionRule.grant
+import ch.epfl.sdp.mobile.state.HomeActivity
 import ch.epfl.sdp.mobile.ui.ar.ArScreen
 import org.junit.Rule
 import org.junit.Test
 
 class SocialScreenTest {
-  @get:Rule val rule = createComposeRule()
+
+  @get:Rule val permissionRule: GrantPermissionRule = grant(Manifest.permission.CAMERA)
+  @get:Rule val rule = createAndroidComposeRule<HomeActivity>()
 
   @Test
-  fun test() {
-
-/*    rule.setContent { ArScreen() }
-
+  fun test() = withCanceledIntents {
+    rule.setContent { ArScreen() }
     rule.onRoot().printToLog("ARScreenTest")
-    rule.onNode(hasAnyDescendant(hasTestTag("AR"))).assertExists()*/
+    rule.onNode(hasAnyDescendant(hasTestTag("AR"))).assertExists()
+  }
+
+  /**
+   * Executes the given [block] by returning an [ActivityResult] with the code
+   * [Activity.RESULT_CANCELED] each time an intent is triggered. This simulates devices which do
+   * not support AR.
+   *
+   * @param block the block of code to execute.
+   */
+  private inline fun withCanceledIntents(block: () -> Unit) {
+    try {
+      init()
+      intending(anyIntent()).respondWith(ActivityResult(Activity.RESULT_CANCELED, null))
+      block()
+    } finally {
+      release()
+    }
   }
 }
