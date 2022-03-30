5c247c9a63f872dcfe55bd14fd05a2091e33f3cd
Lars Barmettler <barmettler.lars@gmail.com>
2022-03-30 17:46:38 +0200

    add first try to fetch games

--- mobile/src/main/java/ch/epfl/sdp/mobile/state/StatefulPlayScreen.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/state/StatefulPlayScreen.kt
@@ -4,8 +4,16 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import ch.epfl.sdp.mobile.application.authentication.AuthenticatedUser
+import ch.epfl.sdp.mobile.application.chess.Match
+import ch.epfl.sdp.mobile.application.chess.notation.serialize
+import ch.epfl.sdp.mobile.state.Loadable.Companion.loaded
 import ch.epfl.sdp.mobile.ui.play.PlayScreen
 import ch.epfl.sdp.mobile.ui.play.PlayScreenState
+import ch.epfl.sdp.mobile.ui.social.ChessMatch
+import ch.epfl.sdp.mobile.ui.social.MatchResult
+import ch.epfl.sdp.mobile.ui.social.Win
+import kotlinx.coroutines.flow.*
 
 /**
  * Player class that holds info about chess player
@@ -15,6 +23,16 @@ import ch.epfl.sdp.mobile.ui.play.PlayScreenState
 class Player(private val newGame: () -> Unit) : PlayScreenState {
   /* TODO: override other attributes */
   override val onNewGameClick = newGame
+}
+
+sealed interface Loadable< out T> {
+  object Loading : Loadable<Nothing>
+  data class Loaded<out T>(val value: T) : Loadable<T>
+
+  companion object {
+    fun loading(): Loadable<Nothing> = Loading
+    fun <T> loaded(value: T): Loadable<T> = Loaded(value)
+  }
 }
 
 /**
@@ -28,7 +46,36 @@ fun StatefulPlayScreen(
     modifier: Modifier = Modifier,
     contentPadding: PaddingValues = PaddingValues(),
 ) {
-  /* TODO: Add any necessary args: authenticated user ... */
+
   val state = remember(navigateToGame) { Player(newGame = navigateToGame) }
   PlayScreen(state, modifier, contentPadding)
+}
+
+fun fetchMatches(match: Match, user: AuthenticatedUser): Flow<Loadable<ChessMatch>> {
+  val black = match.black
+  val white = match.white
+  val game = match.game
+
+  val adversary =
+      black
+          .flatMapMerge { if (user.uid == it?.uid) black else white }
+          .map { it?.name ?: "" }
+          .map { loaded(it) }
+          .onStart { emit(Loadable.Loading) }
+  val movesCount =
+      game.map { it.serialize().size }.map { loaded(it) }.onStart { emit(Loadable.Loading) }
+
+  return combine(adversary, movesCount) { a, m ->
+    Pair(a, m).
+    when {
+      a as Loadable.Loaded<String> ->
+          loaded(ChessMatch(
+              adversary = v, matchResult = Win(MatchResult.Reason.CHECKMATE), numberOfMoves = m))
+      else -> Loadable.Loading
+    }
+  }
+}
+
+fun <Loadable<String>,  Loadable<Int>> Pair<*, *>.ChessMatchAdapter(): Pair<Any, Loadable> {
+
 }
