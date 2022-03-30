5c1a57b5a40a75dc93f4d1372b40b28f0b8e4d85
Matthieu Burguburu <36692720+matt989253@users.noreply.github.com>
2022-03-30 15:04:57 +0200

    Upgrade Chess Facade (#190)
    * Add missing CompositionLocal ChessFacade test
    
    Co-authored-by: Alexandre Piveteau <alexandre@piveteau.email>
    
    * Move ChessDocuments de/serialization to its own file
    
    Co-authored-by: Alexandre Piveteau <alexandre@piveteau.email>
    
    * Rename `fetchMatchesForUser` to `match`
    
    Co-authored-by: Alexandre Piveteau <alexandre@piveteau.email>
    
    * Remove unneeded function on Match companion object
    
    Co-authored-by: Alexandre Piveteau <alexandre@piveteau.email>
    
    * Change `Match` representation to use `Flow`s
    
    Co-authored-by: Alexandre Piveteau <alexandre@piveteau.email>
    
    * Add move tests by clicking and dragging for each piece with each color
    
    * Add move tests for taking a piece and castling both sides
    
    * Add move tests for moves out of bound and game with no playerId
    
    * Remove regression test showing that `ChessBoardRobot`'s `drag()` works since the bug isn't resolved yet.
    
    * Expand test for playing game without specified uids for players
    
    Co-authored-by: Alexandre Piveteau <alexandre@piveteau.email>

--- mobile/src/main/java/ch/epfl/sdp/mobile/application/StoreDocuments.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/application/StoreDocuments.kt
@@ -2,10 +2,7 @@ package ch.epfl.sdp.mobile.application
 
 import ch.epfl.sdp.mobile.application.authentication.AuthenticatedUser
 import ch.epfl.sdp.mobile.application.authentication.AuthenticationUser
-import ch.epfl.sdp.mobile.application.chess.Match
 import ch.epfl.sdp.mobile.application.chess.engine.*
-import ch.epfl.sdp.mobile.application.chess.engine.rules.Action
-import ch.epfl.sdp.mobile.ui.game.ChessBoardCells
 import com.google.firebase.firestore.DocumentId
 
 /**
@@ -55,9 +52,9 @@ fun ProfileDocument?.toProfile(currentUser: AuthenticationUser): Profile {
  * A document which represents a game of chess between two users. All the game documents are stored
  * in the `/games/` collection.
  *
- * @param moves
- * @param whiteId
- * @param blackId
+ * @param moves the list of moves played during the match in long algebraic notation
+ * @param whiteId the UID of the white player
+ * @param blackId The UID of the black player
  */
 data class ChessDocument(
     @DocumentId val uid: String? = null,
@@ -65,101 +62,3 @@ data class ChessDocument(
     val whiteId: String? = null,
     val blackId: String? = null,
 )
-
-/**
- * Deserializes and creates a [Match] from a [ChessDocument]
- *
- * For a null [ChessDocument], a default [Match] with null value and default [Game] positions will
- * be created
- *
- * @return The deserialized [Match]
- */
-fun ChessDocument?.deserialize(): Match {
-  var game = Game.create()
-
-  for (move in this?.moves ?: emptyList()) {
-    val (position, delta) = parseStringToMove(move)
-    game = (game.nextStep as? NextStep.MovePiece)?.move?.invoke(position, delta) ?: game
-  }
-  return Match(game, this?.uid, this?.whiteId, this?.blackId)
-}
-
-/**
- * Serializes a [Match] to a [ChessDocument]
- *
- * @return The serialized [ChessDocument]
- */
-fun Match.serialize(): ChessDocument {
-  val sequence = sequence {
-    var previous = this@serialize.game.previous
-    while (previous != null) {
-      val (game, action) = previous
-      val str = parseMoveToString(game, action)
-      yield(str)
-      previous = game.previous
-    }
-  }
-
-  return ChessDocument(
-      moves = sequence.toList().asReversed(), whiteId = this.whiteId, blackId = this.blackId)
-}
-
-/** Parsing string to moves */
-private fun parseStringToMove(move: String): Pair<Position, Delta> {
-
-  val length = move.length
-  val fromStr = move.subSequence(length - 5, length - 3).toString()
-  val toStr = move.subSequence(length - 2, length).toString()
-
-  val from = chessNotationToPosition(fromStr)
-  val to = chessNotationToPosition(toStr)
-  val delta = Delta(to.x - from.x, to.y - from.y)
-
-  return from to delta
-}
-
-private fun chessNotationToPosition(pos: String): Position {
-  val x = pos[0].code - 'a'.code
-  val y = (ChessBoardCells - 1) - (pos[1].code - '1'.code)
-
-  return Position(x, y)
-}
-
-/** Parsing moves to string */
-private fun parseMoveToString(game: Game, action: Action): String {
-  val (from, delta) = action
-
-  val pieceFrom = game.board[from]
-
-  val to = from.plus(delta)
-  val pieceTo = to?.let { game.board[it] }
-
-  val pieceStr = pieceToChessNotation(pieceFrom)
-  val fromStr = positionToChessNotation(from)
-  val interStr = if (pieceTo == null) "-" else "x"
-  val toStr = positionToChessNotation(to)
-
-  return pieceStr + fromStr + interStr + toStr
-}
-
-private fun pieceToChessNotation(piece: Piece<Color>?): String {
-  return when (piece?.rank) {
-    Rank.King -> "K"
-    Rank.Queen -> "Q"
-    Rank.Rook -> "R"
-    Rank.Bishop -> "B"
-    Rank.Knight -> "N"
-    Rank.Pawn -> ""
-    else -> "?"
-  }
-}
-
-private fun positionToChessNotation(pos: Position?): String {
-  return if (pos == null || !pos.inBounds) {
-    "?"
-  } else {
-    val col = (pos.x.toChar() + 'a'.code).toString()
-    val row = ChessBoardCells - pos.y
-    col + row
-  }
-}
