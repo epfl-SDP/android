96a8c685c2ba4e48ad198fbfa1b4262ce49a2e8f
Alexandre Piveteau <alexandre@piveteau.email>
2022-04-04 00:15:48 +0200

    Improve the `SearchField`

--- mobile/src/main/java/ch/epfl/sdp/mobile/ui/social/SearchField.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/ui/social/SearchField.kt
@@ -1,24 +1,27 @@
 package ch.epfl.sdp.mobile.ui.social
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Icon
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import ch.epfl.sdp.mobile.state.LocalLocalizedStrings
-import ch.epfl.sdp.mobile.ui.Branded
-import ch.epfl.sdp.mobile.ui.Search
+import ch.epfl.sdp.mobile.ui.*
 
 /**
  * An [OutlinedTextField] which displays a centered search placeholder, and which may be used as an
@@ -29,6 +32,7 @@ import ch.epfl.sdp.mobile.ui.Search
  * @param modifier the [Modifier] for this composable.
  * @param interactionSource the [MutableInteractionSource] for this composable.
  */
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SearchField(
     value: String,
@@ -36,10 +40,13 @@ fun SearchField(
     modifier: Modifier = Modifier,
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
 ) {
+  val focused by interactionSource.collectIsFocusedAsState()
+  val (requester) = FocusRequester.createRefs()
+  val controller = LocalSoftwareKeyboardController.current
   OutlinedTextField(
       value = value,
       onValueChange = onValueChange,
-      modifier = modifier.width(IntrinsicSize.Min),
+      modifier = modifier.width(IntrinsicSize.Min).focusRequester(requester),
       placeholder = {
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -48,6 +55,21 @@ fun SearchField(
         ) {
           Icon(Icons.Branded.Search, null)
           Text(LocalLocalizedStrings.current.socialSearchBarPlaceHolder)
+        }
+      },
+      trailingIcon = {
+        AnimatedVisibility(
+            visible = focused,
+            enter = FadeEnterTransition,
+            exit = FadeExitTransition,
+        ) {
+          IconButton(
+              onClick = {
+                // Clear the text and hide the keyboard.
+                onValueChange("")
+                controller?.hide()
+              },
+          ) { Icon(PawniesIcons.GameClose, null) }
         }
       },
       interactionSource = interactionSource,
