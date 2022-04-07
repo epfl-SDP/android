3a47e3dc17e8988f2ea384c2d69298bf6a423414
Lars Barmettler <barmettler.lars@gmail.com>
2022-04-07 20:37:19 +0200

    another formating adjustment

--- mobile/src/main/java/ch/epfl/sdp/mobile/ui/profile/UserScreen.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/ui/profile/UserScreen.kt
@@ -16,8 +16,8 @@ import ch.epfl.sdp.mobile.ui.social.*
  * @param header part of slot construct that comes into the header
  * @param profileBar part of slot construct that represents the tab bar.
  * @param matches the part that is responsible for the list of all matches
- * @param lazyColumnState to keep state of LazyColumn
- * onMatchClick a callback called when a [ChessMatch] is clicked
+ * @param lazyColumnState to keep state of LazyColumn onMatchClick a callback called when a
+ * [ChessMatch] is clicked
  * @param modifier the [Modifier] for this composable.
  */
 @OptIn(ExperimentalFoundationApi::class)
