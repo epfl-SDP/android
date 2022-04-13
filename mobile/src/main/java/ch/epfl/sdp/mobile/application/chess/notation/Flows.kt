e7e4f5833b531b687cf960350270067956da0863
Alexandre Piveteau <alexandre@piveteau.email>
2022-04-13 17:55:43 +0200

    Incrementally update games using the `Flow` of moves (#236)
    * Add an incremental `mapToGame` `Flow` operator
    * Extract the `prefixLength` method
    * Update incremental game update implementation
    * Add some tests for incremental game updates

---
+++ mobile/src/main/java/ch/epfl/sdp/mobile/application/chess/notation/Flows.kt
@@ -0,0 +1,34 @@
+package ch.epfl.sdp.mobile.application.chess.notation
+
+import ch.epfl.sdp.mobile.application.chess.engine.Game
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+/**
+ * Returns the longest common prefix length between two [List].
+ *
+ * @param a the first [List] to inspect.
+ * @param b the second [List] to inspect.
+ */
+private fun prefixLength(a: List<*>, b: List<*>): Int =
+    a.asSequence().zip(b.asSequence()).takeWhile { (a, b) -> (a == b) }.count()
+
+/**
+ * A specialized [Flow] operator which incrementally maps some lists of [String] moves to the
+ * resulting chess [Game].
+ *
+ * @receiver a [Flow] of the list of the moves.
+ * @return a [Flow] of the latest [Game] state.
+ */
+fun Flow<List<String>>.mapToGame(): Flow<Game> = flow {
+  var game = Game.create()
+  var moves = emptyList<String>()
+  collect { list ->
+    val prefix = prefixLength(moves, list)
+    game =
+        if (prefix == moves.size) list.drop(prefix).deserialize(initial = game) // Incremental.
+        else list.deserialize()
+    moves = list
+    emit(game)
+  }
+}
