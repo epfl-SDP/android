8e883aed66302c11fb134dea9a6d610e61a1be7e
Juri <Chau-ying-kot@hotmail.com>
2022-04-15 13:50:40 +0200

    Remove the AR tap in the bottom bar

--- mobile/src/main/java/ch/epfl/sdp/mobile/state/StatefulHome.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/state/StatefulHome.kt
@@ -32,10 +32,7 @@ private const val GameDefaultId = ""
 /** The route associated to new game button in play screen */
 private const val PrepareGameRoute = "prepare_game"
 
-/**
- * The route associated to the ar tab. Note : This tab is temporary, use only for the development
- * TODO : Remove this when we can display the entire game on AR
- */
+/** The route associated to the ar tab. */
 private const val ArRoute = "ar"
 
 /**
@@ -128,7 +125,6 @@ fun StatefulHome(
 private fun NavBackStackEntry.toSection(): HomeSection =
     when (destination.route) {
       SettingsRoute -> HomeSection.Settings
-      ArRoute -> HomeSection.Ar
       PlayRoute -> HomeSection.Play
       else -> HomeSection.Social
     }
@@ -138,7 +134,6 @@ private fun HomeSection.toRoute(): String =
     when (this) {
       HomeSection.Social -> SocialRoute
       HomeSection.Settings -> SettingsRoute
-      HomeSection.Ar -> ArRoute
       HomeSection.Play -> PlayRoute
     }
 
