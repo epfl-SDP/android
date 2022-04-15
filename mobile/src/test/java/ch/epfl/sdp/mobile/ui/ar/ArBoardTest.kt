2cfe8b06e51e4c6487625d21e83f5b3c72903fab
Juri <Chau-ying-kot@hotmail.com>
2022-04-15 13:52:44 +0200

    Fix unit test

--- mobile/src/test/java/ch/epfl/sdp/mobile/ui/ar/ArBoardTest.kt
+++ mobile/src/test/java/ch/epfl/sdp/mobile/ui/ar/ArBoardTest.kt
@@ -1,15 +1,16 @@
 package ch.epfl.sdp.mobile.ui.ar
 
-import ch.epfl.sdp.mobile.application.chess.engine.Position
+import ch.epfl.sdp.mobile.ui.game.ChessBoardState
+import ch.epfl.sdp.mobile.ui.game.ar.ArBoard
 import org.junit.Assert.*
 import org.junit.Test
 
-public class ArBoardTest {
+class ArBoardTest {
   private val board = ArBoard(2f, 1f, 10f)
 
   @Test
   fun `Ensure that the position is translated correctly`() {
-    val p = Position(1, 1)
+    val p = ChessBoardState.Position(1, 1)
     val arPosition = board.toArPosition(p)
 
     assertEquals(-5f, arPosition.x)
