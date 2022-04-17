b9f365471709907fe49ef33ed0fdfa9eb0730d48
theMaimu <barmettler.lars@gmail.com>
2022-04-17 21:33:22 +0200

    Update mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/state/StatefulHomeTest.kt
    Co-authored-by: Alexandre Piveteau <alexandre@piveteau.email>

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/state/StatefulHomeTest.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/state/StatefulHomeTest.kt
@@ -335,11 +335,9 @@ class StatefulHomeTest {
 
     val strings =
         rule.setContentWithLocalizedStrings {
-          val controller = rememberNavController()
           ProvideFacades(authFacade, socialFacade, chessFacade) {
             StatefulHome(
                 user = user,
-                controller = controller,
             )
           }
         }
