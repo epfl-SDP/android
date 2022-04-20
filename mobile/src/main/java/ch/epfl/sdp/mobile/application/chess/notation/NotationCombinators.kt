3bbc163d93c4db44bbf5c1a09fd124b762b5631a
Alexandre Piveteau <alexandre@piveteau.email>
2022-04-20 13:45:23 +0200

    Use backing fields in `NotationCombinators`

--- mobile/src/main/java/ch/epfl/sdp/mobile/application/chess/notation/NotationCombinators.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/application/chess/notation/NotationCombinators.kt
@@ -20,20 +20,20 @@ import ch.epfl.sdp.mobile.application.chess.parser.StringCombinators.digit
 object NotationCombinators {
 
   /** A [Parser] which returns the column in a position. */
-  private fun column(): Parser<String, Int> = char().filter { it in 'a'..'h' }.map { it - 'a' }
+  private val column = char().filter { it in 'a'..'h' }.map { it - 'a' }
 
   /** A [Parser] which returns the row in a position. */
-  private fun row(): Parser<String, Int> = digit().map { 8 - it }
+  private val row = digit().map { 8 - it }
 
   /** A [Parser] which returns a [Position]. */
-  private fun position(): Parser<String, Position> =
-      column().flatMap { x -> row().map { y -> Position(x, y) } }.filter { it.inBounds }
+  private val position =
+      column.flatMap { x -> row.map { y -> Position(x, y) } }.filter { it.inBounds }
 
   /**
    * A [Parser] which returns the [Rank] of a piece, and defaults to [Rank.Pawn] if no matching rank
    * letter was found.
    */
-  private fun rank(): Parser<String, Rank> =
+  private val rank =
       combine(
           char('K').map { Rank.King },
           char('Q').map { Rank.Queen },
@@ -43,29 +43,30 @@ object NotationCombinators {
       )
 
   /** A [Parser] which eats a `-` or an `x` character. */
-  private fun moveSeparator(): Parser<String, Char> = char('-') or char('x')
+  private val separator = char('-') or char('x')
 
   /** A [Parser] for a [Move] action. */
-  private fun move(): Parser<String, Move> =
-      rank()
+  private val move =
+      rank
           .orElse { Rank.Pawn }
           .flatMap {
-            position().flatMap { from ->
-              moveSeparator().flatMap { position().map { to -> Move(from, to) } }
-            }
+            position.flatMap { from -> separator.flatMap { position.map { to -> Move(from, to) } } }
           }
           .checkFinished()
 
   /** A [Parser] for a [Promote] action. */
-  private fun promote(): Parser<String, Promote> =
-      position() // No leading rank because only pawns may be promoted.
+  private val promote =
+      position // No leading rank because only pawns may be promoted.
           .flatMap { from ->
-            moveSeparator().flatMap {
-              position().flatMap { to -> rank().map { rank -> Promote(from, to, rank) } }
+            separator.flatMap {
+              position.flatMap { to -> rank.map { rank -> Promote(from, to, rank) } }
             }
           }
           .checkFinished()
 
-  /** A [Parser] for an [Action]. */
-  fun action(): Parser<String, Action> = combine(move(), promote())
+  /** A [Parser] for an action. */
+  private val action = combine(move, promote)
+
+  /** Returns a [Parser] for an [Action]. */
+  fun action(): Parser<String, Action> = action
 }
