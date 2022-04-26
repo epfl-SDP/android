4ae8d038eb6d8c15114dbbdf6bfb35d5df2672b1
Chau Ying Kot <chau-ying-kot@hotmail.com>
2022-04-26 09:47:25 +0200

    Refactor : Use state in AR Chess board (#251)
    Now the AR view use a state to observe the state of a specific game. Thus, the button in the bottom bar is removed.
    To access to the AR view, we can click on the AR button on the 2D game screen. 
    
    
    
    Co-authored-by: Alexandre Piveteau <alexandre@piveteau.email>
    Co-authored-by: Matthieu Burguburu <36692720+matt989253@users.noreply.github.com>

--- mobile/src/main/java/ch/epfl/sdp/mobile/state/StatefullArScreen.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/state/StatefullArScreen.kt
@@ -1,17 +1,42 @@
 package ch.epfl.sdp.mobile.state
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import ch.epfl.sdp.mobile.application.chess.engine.Game
-import ch.epfl.sdp.mobile.ui.ar.ArScreen
+import ch.epfl.sdp.mobile.application.authentication.AuthenticatedUser
+import ch.epfl.sdp.mobile.ui.game.ar.ArChessBoardScreen
 
+/**
+ * A composable that make [ArChessBoardScreen] stateful
+ *
+ * @param id the identifier for the match.
+ * @param modifier the [Modifier] for the composable.
+ */
 @Composable
 fun StatefulArScreen(
+    user: AuthenticatedUser,
+    id: String,
     modifier: Modifier = Modifier,
 ) {
 
-  // FIX ME : Take the game for a give id
-  val game = Game.create()
+  val facade = LocalChessFacade.current
+  val scope = rememberCoroutineScope()
+  val match = remember(facade, id) { facade.match(id) }
 
-  ArScreen(game, modifier)
+  // FIXME : Not the cleanest code, but the faster way to deal with state with different UI. The AR
+  //  Screen didn't need all methods in [SnapshotChessBoardState]. The cleaner way is to have a
+  //  simpler snapshot that contains only information that will be needed by the
+  //  [ArChessBoardScreen]
+  val actions = StatefulGameScreenActions({}, {})
+
+  val gameScreenState =
+      remember(actions, user, match, scope) {
+        SnapshotChessBoardState(
+            actions = actions,
+            user = user,
+            match = match,
+            scope = scope,
+        )
+      }
+
+  ArChessBoardScreen(gameScreenState, modifier)
 }
