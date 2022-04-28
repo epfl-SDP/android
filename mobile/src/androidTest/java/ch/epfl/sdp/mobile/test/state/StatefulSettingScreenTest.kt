7afe98bb3839e30eb8f093dc2027b7721e8fa134
Fouad Mahmoud <fouad.mahmoud@epfl.ch>
2022-04-29 01:14:55 +0200

    fix second test

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/state/StatefulSettingScreenTest.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/state/StatefulSettingScreenTest.kt
@@ -5,7 +5,13 @@ import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.performClick
 import ch.epfl.sdp.mobile.application.Profile
 import ch.epfl.sdp.mobile.application.authentication.AuthenticatedUser
+import ch.epfl.sdp.mobile.application.authentication.AuthenticationFacade
+import ch.epfl.sdp.mobile.application.chess.ChessFacade
+import ch.epfl.sdp.mobile.application.social.SocialFacade
+import ch.epfl.sdp.mobile.state.ProvideFacades
 import ch.epfl.sdp.mobile.state.StatefulSettingsScreen
+import ch.epfl.sdp.mobile.test.infrastructure.persistence.auth.emptyAuth
+import ch.epfl.sdp.mobile.test.infrastructure.persistence.store.emptyStore
 import com.google.common.truth.Truth.assertThat
 import io.mockk.every
 import io.mockk.mockk
@@ -30,9 +36,15 @@ class StatefulSettingScreenTest {
 
     val openProfileEditNameMock = { functionCalled = true }
 
+    val authFacade = AuthenticationFacade(emptyAuth(), emptyStore())
+    val socialFacade = SocialFacade(emptyAuth(), emptyStore())
+    val chessFacade = ChessFacade(emptyAuth(), emptyStore())
+
     val strings =
         rule.setContentWithLocalizedStrings {
-          StatefulSettingsScreen(user, openProfileEditNameMock)
+          ProvideFacades(authFacade, socialFacade, chessFacade) {
+            StatefulSettingsScreen(user, openProfileEditNameMock)
+          }
         }
 
     rule.onNodeWithContentDescription(strings.profileEditNameIcon).performClick()
