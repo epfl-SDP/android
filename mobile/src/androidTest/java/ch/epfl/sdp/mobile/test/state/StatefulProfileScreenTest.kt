83a2cbae730b7cd91727e807873cb2e36e50122b
Fouad Mahmoud <fouad.mahmoud@epfl.ch>
2022-04-28 23:33:38 +0200

    add test to null state in StatefulProfileScreen

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/state/StatefulProfileScreenTest.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/state/StatefulProfileScreenTest.kt
@@ -1,6 +1,8 @@
 package ch.epfl.sdp.mobile.test.state
 
+import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithText
 import ch.epfl.sdp.mobile.application.ChessDocument
 import ch.epfl.sdp.mobile.application.ProfileDocument
@@ -43,6 +45,24 @@ class StatefulProfileScreenTest {
             ProvideFacades(authFacade, socialFacade, chessFacade) { StatefulProfileScreen("1") }
           }
       rule.onNodeWithText(strings.profileMatchTitle("B")).assertExists()
+    }
+  }
+
+  @Test
+  fun given_statefulProfileScreen_when_profileStateIsNull_then_aNullProfileIsDisplayed() {
+    runTest {
+      val auth = buildAuth { user("email@example.org", "password", "1") }
+      val store = buildStore {
+        collection("users") { document("1", ProfileDocument("1", name = "B")) }
+      }
+      val authFacade = AuthenticationFacade(auth, store)
+      val socialFacade = SocialFacade(auth, store)
+      val chessFacade = ChessFacade(auth, store)
+      rule.setContentWithLocalizedStrings {
+        ProvideFacades(authFacade, socialFacade, chessFacade) { StatefulProfileScreen("2") }
+      }
+
+      rule.onAllNodesWithText("").assertCountEquals(3)
     }
   }
 }
