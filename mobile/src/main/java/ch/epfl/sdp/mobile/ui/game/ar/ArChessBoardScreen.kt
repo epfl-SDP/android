6926d242ae546371add219c65064f8e6f7f703f3
Juri <Chau-ying-kot@hotmail.com>
2022-04-29 20:37:39 +0200

    Move `anchorOrMoveBoard` outside of `AndroidView`

--- mobile/src/main/java/ch/epfl/sdp/mobile/ui/game/ar/ArChessBoardScreen.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/ui/game/ar/ArChessBoardScreen.kt
@@ -36,6 +36,25 @@ fun <Piece : ChessBoardState.Piece> ArChessBoardScreen(
     onDispose { view.keepScreenOn = false }
   }
 
+  /**
+   * If not already in the scene, the board will be added. Update the board anchor with the given
+   * one and change the displayed position
+   *
+   * @param anchor The (new) board's anchor position
+   */
+  fun anchorOrMoveBoard(arSceneView: ArSceneView, anchor: Anchor) {
+
+    val currentChessScene = chessScene ?: return
+
+    currentChessScene.let {
+      // Add only one instance of the node
+      if (!arSceneView.children.contains(it.boardNode)) {
+        arSceneView.addChild(it.boardNode)
+      }
+      it.boardNode.anchor = anchor
+    }
+  }
+
   AndroidView(
       factory = { context ->
 
@@ -48,27 +67,10 @@ fun <Piece : ChessBoardState.Piece> ArChessBoardScreen(
               scale(BoardScale)
             }
 
-        /**
-         * If not already in the scene, the board will be added. Update the board anchor with the
-         * given one and change the displayed position
-         *
-         * @param anchor The (new) board's anchor position
-         */
-        fun anchorOrMoveBoard(anchor: Anchor) {
-
-          val currentChessScene = chessScene ?: return
-
-          currentChessScene.let {
-            // Add only one instance of the node
-            if (!arSceneView.children.contains(it.boardNode)) {
-              arSceneView.addChild(it.boardNode)
-            }
-            it.boardNode.anchor = anchor
-          }
-        }
-
         // Place the chess board on the taped position
-        arSceneView.onTouchAr = { hitResult, _ -> anchorOrMoveBoard(hitResult.createAnchor()) }
+        arSceneView.onTouchAr = { hitResult, _ ->
+          anchorOrMoveBoard(arSceneView, hitResult.createAnchor())
+        }
 
         arSceneView
       },
