2ad0cb7b5d3d1a2a86ac17d4d5a37c553a058d05
matt989253 <matthieu.burguburu@gmail.com>
2022-05-01 20:53:16 +0200

    Add UCI Parser

--- mobile/src/main/java/ch/epfl/sdp/mobile/application/chess/notation/UCINotation.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/application/chess/notation/UCINotation.kt
@@ -3,9 +3,7 @@ package ch.epfl.sdp.mobile.application.chess.notation
 import ch.epfl.sdp.mobile.application.chess.engine.rules.Action
 import ch.epfl.sdp.mobile.application.chess.notation.UCINotationCombinators.uciActions
 
-/**
- * An object which contains some utilities to transform games in UCI notation, and vice-versa.
- */
+/** An object which contains some utilities to transform games in UCI notation, and vice-versa. */
 object UCINotation {
 
   /**
