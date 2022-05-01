f74f5e7fa17647a3ccf81239e93430f0cff0121f
matt989253 <matthieu.burguburu@gmail.com>
2022-05-01 16:03:29 +0200

    Refactor generic notation combinators to it own object

---
+++ mobile/src/main/java/ch/epfl/sdp/mobile/application/chess/notation/GenericNotationCombinators.kt
@@ -0,0 +1,23 @@
+package ch.epfl.sdp.mobile.application.chess.notation
+
+import ch.epfl.sdp.mobile.application.chess.engine.Position
+import ch.epfl.sdp.mobile.application.chess.parser.Combinators.filter
+import ch.epfl.sdp.mobile.application.chess.parser.Combinators.flatMap
+import ch.epfl.sdp.mobile.application.chess.parser.Combinators.map
+import ch.epfl.sdp.mobile.application.chess.parser.Parser
+import ch.epfl.sdp.mobile.application.chess.parser.StringCombinators
+
+/**
+ * An object which contains some convenience parser combinators for any notation
+ */
+object GenericNotationCombinators {
+  /** A [Parser] which returns the column in a position. */
+  val column = StringCombinators.char().filter { it in 'a'..'h' }.map { it - 'a' }
+
+  /** A [Parser] which returns the row in a position. */
+  val row = StringCombinators.digit().map { 8 - it }
+
+  /** A [Parser] which returns a [Position]. */
+  val position =
+    column.flatMap { x -> row.map { y -> Position(x, y) } }.filter { it.inBounds }
+}
