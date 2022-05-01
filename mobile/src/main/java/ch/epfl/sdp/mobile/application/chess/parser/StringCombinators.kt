2be4b8ad528306234ad231144191f4e61f4d1112
matt989253 <matthieu.burguburu@gmail.com>
2022-05-01 16:05:11 +0200

    Add a token parser and lowerCase char parser to `StringCombinators`

--- mobile/src/main/java/ch/epfl/sdp/mobile/application/chess/parser/StringCombinators.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/application/chess/parser/StringCombinators.kt
@@ -20,8 +20,36 @@ object StringCombinators {
    */
   fun char(value: Char): Parser<String, Char> = char().filter { it == value }
 
+  /**
+   * Parses the first [Char] of a [String], if it's not empty and has the provided value whether it
+   * is lower-case of upper-case.
+   *
+   * @param value the value that is searched.
+   */
+  fun charLower(value: Char): Parser<String, Char> = char().filter { it.lowercaseChar() == value }
+
   /** Parses the first digit of a [String], if it exists. */
   fun digit(): Parser<String, Int> = char().filter { it in '0'..'9' }.map { it - '0' }
+
+  /** Parses the first token of a [String], if it's exists.
+   *
+   * @param delimiter The [String] expected between tokens
+   * @param ignoreCase Whether or not to ignore case in delimiters
+   */
+  fun token(
+      delimiter: String,
+      ignoreCase: Boolean = false,
+  ): Parser<String, String> = Parser {
+    val tokens = it.split(delimiter, ignoreCase = ignoreCase, limit = 1)
+    if(tokens.isNotEmpty()) {
+      val token = tokens.first()
+      val toDrop = it.indexOf(token) + token.length
+
+      setOf(Parser.Result(it.drop(toDrop), token))
+    } else {
+      emptySet()
+    }
+  }
 
   /**
    * Filters the results from this [Parser] which have an empty remaining input to parse,
