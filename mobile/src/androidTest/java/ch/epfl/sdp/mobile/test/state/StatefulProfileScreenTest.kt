7b75eba40810c8201edf96fe4418ee1e9fc5d204
Fouad Mahmoud <fouad.mahmoud@epfl.ch>
2022-05-02 17:42:07 +0200

    remove test

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/state/StatefulProfileScreenTest.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/state/StatefulProfileScreenTest.kt
@@ -1,8 +1,6 @@
 package ch.epfl.sdp.mobile.test.state
 
-import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithText
 import ch.epfl.sdp.mobile.application.ChessDocument
 import ch.epfl.sdp.mobile.application.ProfileDocument
@@ -47,24 +45,6 @@ class StatefulProfileScreenTest {
             }
           }
       rule.onNodeWithText(strings.profileMatchTitle("B")).assertExists()
-    }
-  }
-
-  @Test
-  fun given_statefulProfileScreen_when_profileStateIsNull_then_anEmptyProfileIsDisplayed() {
-    runTest {
-      val auth = buildAuth { user("email@example.org", "password", "1") }
-      val store = buildStore {
-        collection("users") { document("1", ProfileDocument("1", name = "B")) }
-      }
-      val authFacade = AuthenticationFacade(auth, store)
-      val socialFacade = SocialFacade(auth, store)
-      val chessFacade = ChessFacade(auth, store)
-      rule.setContentWithLocalizedStrings {
-        ProvideFacades(authFacade, socialFacade, chessFacade) { StatefulVisitedProfileScreen("2") }
-      }
-
-      rule.onAllNodesWithText("").assertCountEquals(2)
     }
   }
 }
