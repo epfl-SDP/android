c3e87ed7a2445477dfb4294e00be6d6679ab726d
Alexandre Piveteau <alexandre@piveteau.email>
2022-05-02 12:22:04 +0200

    Add support for nested objects to `FakeDocumentRecord.toObject()`

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/infrastructure/persistence/store/DslNestingTest.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/infrastructure/persistence/store/DslNestingTest.kt
@@ -10,6 +10,7 @@ import org.junit.Test
 class DslNestingTest {
 
   data class User(
+      val id: String? = null,
       val profile: Profile? = null,
   )
 
@@ -21,9 +22,12 @@ class DslNestingTest {
   @Test
   fun given_emptyStore_when_creatingDocumentWithNestedFields_then_succeeds() = runTest {
     val reference = emptyStore().collection("hello").document()
-    reference.set { this["user"] = mapOf("name" to "Alexandre", "age" to 24) }
+    reference.set {
+      this["id"] = "alex"
+      this["user"] = mapOf("name" to "Alexandre", "age" to 24)
+    }
     val user = reference.asFlow<User>().filterNotNull().first()
 
-    assertThat(user).isEqualTo(User(Profile("Alexandre", 24)))
+    assertThat(user).isEqualTo(User("alex", Profile("Alexandre", 24)))
   }
 }
--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/infrastructure/persistence/store/fake/serialization/Records.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/infrastructure/persistence/store/fake/serialization/Records.kt
@@ -8,6 +8,7 @@ import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
 import kotlin.reflect.full.memberProperties
 import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.jvmErasure
 import kotlin.reflect.typeOf
 
 /**
@@ -29,6 +30,33 @@ private inline fun <reified A : Annotation> KClass<*>.hasJavaAnnotatedField(name
 /** The set of [kotlin.reflect.KType] that will be accepted for [DocumentId]-annotated fields. */
 private val SupportedDocumentIdTypes = setOf(typeOf<String>(), typeOf<String?>())
 
+/** The set of [kotlin.reflect.KType] that will be accepted as primitive values for documents. */
+private val SupportedFieldTypes =
+    setOf(
+        typeOf<Boolean>(),
+        typeOf<Boolean?>(),
+        typeOf<Double>(),
+        typeOf<Double?>(),
+        typeOf<Float>(),
+        typeOf<Float?>(),
+        typeOf<Int>(),
+        typeOf<Int?>(),
+        typeOf<Long>(),
+        typeOf<Long?>(),
+        typeOf<Number>(),
+        typeOf<Number?>(),
+        typeOf<Short>(),
+        typeOf<Short?>(),
+        typeOf<String>(),
+        typeOf<String?>(),
+        // Collections.
+        typeOf<List<*>>(),
+        typeOf<List<*>?>(),
+        // If the types are not specified, we can't automatically map them to their data classes.
+        typeOf<Any>(),
+        typeOf<Any?>(),
+    )
+
 /**
  * Returns an object of type [T] using the provided [FakeDocumentRecord].
  *
@@ -41,23 +69,47 @@ private val SupportedDocumentIdTypes = setOf(typeOf<String>(), typeOf<String?>()
  * @return the T corresponding to the object to be built.
  */
 fun <T : Any> FakeDocumentRecord.toObject(id: FakeDocumentId, valueClass: KClass<T>): T {
+  return fields.toObject(id, valueClass)
+}
+
+/**
+ * Returns an object of type [T] using the provided [Map] of [FieldPath] to their associated values.
+ *
+ * @param T the type of the returned object.
+ * @receiver the [Map] of field paths which is transformed.
+ * @param id the unique identifier for this document.
+ * @param valueClass the [KClass] of the object to build.
+ *
+ * @return the [T] corresponding to the object to be built.
+ */
+private fun <T : Any> Map<FieldPath, Any?>.toObject(id: FakeDocumentId, valueClass: KClass<T>): T {
   require(valueClass.isData) { "Only data classes are currently supported." }
   val constructor = requireNotNull(valueClass.primaryConstructor) { "Missing a constructor." }
   val arguments = mutableMapOf<KParameter, Any?>()
   for (parameter in constructor.parameters) {
     val name = requireNotNull(parameter.name) { "Unnamed constructor parameter not supported." }
-    if (valueClass.hasJavaAnnotatedField<DocumentId>(name)) {
-      require(parameter.type in SupportedDocumentIdTypes) { "DocumentId must be a String?." }
-      require(fields[FieldPath(name)] == null) {
-        "Found a document field with the same name as DocumentId."
+    when {
+      valueClass.hasJavaAnnotatedField<DocumentId>(name) -> {
+        require(parameter.type in SupportedDocumentIdTypes) { "DocumentId must be a String?." }
+        require(this[FieldPath(name)] == null) {
+          "Found a document field with the same name as DocumentId."
+        }
+        arguments[parameter] = id.value
       }
-      arguments[parameter] = id.value
-    } else {
-      arguments[parameter] = fields[FieldPath(name)]
+      parameter.type.classifier in SupportedFieldTypes.map { it.classifier } ->
+          arguments[parameter] = this[FieldPath(name)]
+      else -> arguments[parameter] = tail().toObject(id, parameter.type.jvmErasure)
     }
   }
   return constructor.callBy(arguments)
 }
+
+/** Returns the tail of the [FieldPath], if it has at least two segments. */
+private fun FieldPath.tail(): FieldPath? =
+    if (segments.size > 1) FieldPath(segments.drop(1)) else null
+
+private fun Map<FieldPath, Any?>.tail(): Map<FieldPath, Any?> =
+    mapKeys { (k) -> k.tail() }.filterKeys { it != null }.mapKeys { (k) -> requireNotNull(k) }
 
 /**
  * Returns a [FakeDocumentRecord] of using the provided [value].
