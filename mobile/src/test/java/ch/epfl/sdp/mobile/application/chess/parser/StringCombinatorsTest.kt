228dfee64718c286e52e8834830de0b6409edb32
ChauYingKot <chau.kot@epfl.ch>
2022-05-03 17:00:37 +0200

    Fix tests name

--- mobile/src/test/java/ch/epfl/sdp/mobile/application/chess/parser/StringCombinatorsTest.kt
+++ mobile/src/test/java/ch/epfl/sdp/mobile/application/chess/parser/StringCombinatorsTest.kt
@@ -7,28 +7,28 @@ import org.junit.Test
 class StringCombinatorsTest {
 
   @Test
-  fun given_aString_when_parsed_then_returnSetOfResult() {
+  fun given_aString_when_parsed_then_returnSequenceOfResult() {
     val res = token().parse("This is a test")
     val expected = Parser.Result("is a test", "This")
     assertThat(res.first()).isEqualTo(expected)
   }
 
   @Test
-  fun given_aString_when_parsedWithAGiven1stWord_then_returnSetOfResult() {
+  fun given_aString_when_parsedWithAGiven1stWord_then_returnSequenceOfResult() {
     val res = token("This").parse("This is a test")
     val expected = Parser.Result("is a test", "This")
     assertThat(res.first()).isEqualTo(expected)
   }
 
   @Test
-  fun given_aEmptyString_when_parsed_then_returnEmptySet() {
+  fun given_aEmptyString_when_parsed_then_returnEmptySequence() {
     val res = token().parse("")
     val expected = emptySequence<Parser<String, Token>>()
     assertThat(res).isEqualTo(expected)
   }
 
   @Test
-  fun given_aString_when_parsedWithAGivenToken_then_returnEmptySet() {
+  fun given_aString_when_parsedWithAGivenToken_then_returnEmptySequence() {
     val res = token("That").parse("This is a test")
     assertThat(res.count()).isEqualTo(0)
   }
