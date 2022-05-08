48745e40d1a4ae01a5f87e43750ddf56ddedd79d
Alexandre Piveteau <alexandre@piveteau.email>
2022-05-08 23:38:41 +0200

    Add a simple test for `PoolCard` with content

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/ui/tournaments/PoolCardTest.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/ui/tournaments/PoolCardTest.kt
@@ -1,13 +1,12 @@
 package ch.epfl.sdp.mobile.test.ui.tournaments
 
-import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
 import ch.epfl.sdp.mobile.test.state.setContentWithLocalizedStrings
 import ch.epfl.sdp.mobile.ui.tournaments.PoolCard
 import ch.epfl.sdp.mobile.ui.tournaments.PoolInfo
 import ch.epfl.sdp.mobile.ui.tournaments.PoolMember
+import ch.epfl.sdp.mobile.ui.tournaments.PoolScore
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.channels.Channel
 import org.junit.Rule
@@ -94,5 +93,31 @@ class PoolCardTest {
         }
     rule.setContentWithLocalizedStrings { PoolCard(info) }
     rule.onNodeWithText("Hello there").assertIsDisplayed()
+  }
+
+  @Test
+  fun given_nonEmptyPool_whenDisplayed_then_showsAllPlayersTwice() {
+    val info =
+        object : PoolInfo<PoolMember> {
+          override val name = "Hello there"
+          override val status = PoolInfo.Status.StillOpen
+          override val startNextRoundEnabled = false
+          override fun onStartNextRound() = Unit
+          override val members =
+              listOf(
+                  object : PoolMember {
+                    override val name: String = "Alexandre"
+                    override val total: PoolScore? = null
+                  },
+                  object : PoolMember {
+                    override val name: String = "Georges"
+                    override val total: PoolScore? = null
+                  },
+              )
+          override fun PoolMember.scoreAgainst(other: PoolMember) = 0
+        }
+    rule.setContentWithLocalizedStrings { PoolCard(info) }
+    rule.onAllNodesWithText("Alexandre", ignoreCase = true).assertCountEquals(2)
+    rule.onAllNodesWithText("Georges", ignoreCase = true).assertCountEquals(2)
   }
 }
