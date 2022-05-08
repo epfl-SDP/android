d678b3f498c2936283003b7a24aea2796b21ef6c
Alexandre Piveteau <alexandre@piveteau.email>
2022-05-09 00:12:17 +0200

    Add a bunch of tests to `TournamentsDetailsTest`

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/ui/tournaments/TournamentsDetailsTest.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/ui/tournaments/TournamentsDetailsTest.kt
@@ -24,8 +24,23 @@ class TournamentsDetailsTest {
     override fun PoolMember.scoreAgainst(other: PoolMember): PoolScore? = null
   }
 
+  class OneMatchTournamentDetails(
+      val match: TournamentMatch,
+  ) : TournamentDetailsState<PoolMember, TournamentMatch> {
+    var clicked = false
+    override val badge: BadgeType? = null
+    override val title = ""
+    override val pools = listOf(EmptyPoolInfo)
+    override val finals = listOf(TournamentsFinalsRound("TAB", listOf(match)))
+    override fun onBadgeClick() = Unit
+    override fun onWatchMatchClick(match: TournamentMatch) {
+      clicked = true
+    }
+    override fun onCloseClick() = Unit
+  }
+
   @Test
-  fun given_tournamentsDetails_when_started_thenDisplaysPoolsTab() {
+  fun given_tournamentsDetails_when_started_then_DisplaysPoolsTab() {
     val details =
         object : TournamentDetailsState<PoolMember, TournamentMatch> {
           override val badge: BadgeType? = null
@@ -41,7 +56,7 @@ class TournamentsDetailsTest {
   }
 
   @Test
-  fun given_tournamentsDetails_when_clickingOnJoinBadge_thenCallsCallback() {
+  fun given_tournamentsDetails_when_clickingOnJoinBadge_then_CallsCallback() {
     val channel = Channel<Unit>(1)
     val details =
         object : TournamentDetailsState<PoolMember, TournamentMatch> {
@@ -58,5 +73,64 @@ class TournamentsDetailsTest {
     val strings = rule.setContentWithLocalizedStrings { TournamentDetails(details) }
     rule.onNodeWithText(strings.tournamentsBadgeJoin).performClick()
     assertThat(channel.tryReceive().getOrNull()).isEqualTo(Unit)
+  }
+
+  @Test
+  fun given_oneOngoingMatch_when_clickingWatch_then_callCallback() {
+    val match =
+        object : TournamentMatch {
+          override val firstPlayerName = "Alexandre"
+          override val secondPlayerName = "Matthieu"
+          override val result = TournamentMatch.Result.Ongoing
+        }
+    val details = OneMatchTournamentDetails(match)
+    val strings = rule.setContentWithLocalizedStrings { TournamentDetails(details) }
+    rule.onNodeWithText("TAB").performClick()
+    rule.onNodeWithText(strings.tournamentsDetailsWatch).performClick()
+    assertThat(details.clicked).isTrue()
+  }
+
+  @Test
+  fun given_oneDrawnMatch_when_displayingFinals_then_displaysCorrectText() {
+    val match =
+        object : TournamentMatch {
+          override val firstPlayerName = "Alexandre"
+          override val secondPlayerName = "Matthieu"
+          override val result = TournamentMatch.Result.Draw
+        }
+    val details = OneMatchTournamentDetails(match)
+    val strings = rule.setContentWithLocalizedStrings { TournamentDetails(details) }
+    rule.onNodeWithText("TAB").performClick()
+    rule.onNodeWithText(strings.tournamentsDetailsMatchDrawn).assertIsDisplayed()
+  }
+
+  @Test
+  fun given_oneWonForFirstMatch_when_displayingFinals_then_displaysCorrectText() {
+    val match =
+        object : TournamentMatch {
+          override val firstPlayerName = "Alexandre"
+          override val secondPlayerName = "Matthieu"
+          override val result = TournamentMatch.Result.FirstWon
+        }
+    val details = OneMatchTournamentDetails(match)
+    val strings = rule.setContentWithLocalizedStrings { TournamentDetails(details) }
+    rule.onNodeWithText("TAB").performClick()
+    rule.onNodeWithText(strings.tournamentsDetailsMatchWon).assertIsDisplayed()
+    rule.onNodeWithText(strings.tournamentsDetailsMatchLost).assertIsDisplayed()
+  }
+
+  @Test
+  fun given_oneLostForFirstMatch_when_displayingFinals_then_displaysCorrectText() {
+    val match =
+        object : TournamentMatch {
+          override val firstPlayerName = "Alexandre"
+          override val secondPlayerName = "Matthieu"
+          override val result = TournamentMatch.Result.SecondWon
+        }
+    val details = OneMatchTournamentDetails(match)
+    val strings = rule.setContentWithLocalizedStrings { TournamentDetails(details) }
+    rule.onNodeWithText("TAB").performClick()
+    rule.onNodeWithText(strings.tournamentsDetailsMatchWon).assertIsDisplayed()
+    rule.onNodeWithText(strings.tournamentsDetailsMatchLost).assertIsDisplayed()
   }
 }
