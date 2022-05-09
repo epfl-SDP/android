eef83a449ed66750e93a236e57fdc044cacb91a7
Alexandre Piveteau <alexandre@piveteau.email>
2022-05-09 22:22:57 +0200

    Add `Transaction` support to `FirestoreStore` and `FakeStore`

--- mobile/src/main/java/ch/epfl/sdp/mobile/infrastructure/persistence/store/Store.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/infrastructure/persistence/store/Store.kt
@@ -8,4 +8,16 @@ interface Store {
 
   /** Accesses a collection with the given [String] identifier. */
   fun collection(path: String): CollectionReference
+
+  /**
+   * Runs the give [Transaction] on the [Store], executing all the operations atomically. A
+   * [Transaction] must perform all its reads before performing all of its writes.
+   *
+   * This function may throw an [Exception] if a transaction can't be executed.
+   *
+   * @param R the type of the return value.
+   * @param block the body of the [Transaction] to be executed.
+   * @return the return value of the transaction.
+   */
+  suspend fun <R> transaction(block: Transaction<DocumentReference>.() -> R): R
 }
--- mobile/src/main/java/ch/epfl/sdp/mobile/infrastructure/persistence/store/firestore/FirestoreStore.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/infrastructure/persistence/store/firestore/FirestoreStore.kt
@@ -1,8 +1,11 @@
 package ch.epfl.sdp.mobile.infrastructure.persistence.store.firestore
 
 import ch.epfl.sdp.mobile.infrastructure.persistence.store.CollectionReference
+import ch.epfl.sdp.mobile.infrastructure.persistence.store.DocumentReference
 import ch.epfl.sdp.mobile.infrastructure.persistence.store.Store
+import ch.epfl.sdp.mobile.infrastructure.persistence.store.Transaction
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
 
 /**
  * An implementation of [Store] which uses Firestore under-the-hood.
@@ -15,4 +18,8 @@ class FirestoreStore(
 
   override fun collection(path: String): CollectionReference =
       FirestoreCollectionReference(firestore.collection(path))
+
+  override suspend fun <R> transaction(
+      block: Transaction<DocumentReference>.() -> R,
+  ): R = firestore.runTransaction { block(FirestoreTransaction(it)) }.await()
 }
