230235d121feec2093a19b5e3c2f9cc947c8cc9b
Alexandre Piveteau <alexandre@piveteau.email>
2022-05-09 10:23:46 +0200

    Add a `DialogChoices` composable

--- mobile/src/main/java/ch/epfl/sdp/mobile/ui/tournaments/CreateDialog.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/ui/tournaments/CreateDialog.kt
@@ -4,6 +4,8 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement.Absolute.spacedBy
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -12,8 +14,8 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType.Companion.Number
+import androidx.compose.ui.text.style.TextOverflow.Companion.Ellipsis
 import androidx.compose.ui.unit.dp
 import ch.epfl.sdp.mobile.state.LocalLocalizedStrings
 import ch.epfl.sdp.mobile.ui.PawniesColors
@@ -22,12 +24,23 @@ import ch.epfl.sdp.mobile.ui.PawniesColors.Green100
 import ch.epfl.sdp.mobile.ui.PawniesColors.Green200
 import ch.epfl.sdp.mobile.ui.prepare_game.Dialog
 
+/** An interface which can be used to hoist the state of a [CreateDialog]. */
 @Stable
 interface CreateDialogState {
+
+  /** The name of the newly created tournament. */
   var name: String
+
+  /** The maximum number of players for the tournament. */
   var maximumPlayerCount: String
+
+  /** True iff the confirm action is enabled. */
   val confirmEnabled: Boolean
+
+  /** A callback which will be called when the confirm action is pressed. */
   fun onConfirm()
+
+  /** A callback which will be called when the dismiss action is pressed. */
   fun onCancel()
 }
 
@@ -81,8 +94,18 @@ fun CreateDialog(
               maxLines = 1,
               keyboardOptions = KeyboardOptions(keyboardType = Number),
           )
-          // TODO : Pool size
-          // TODO : Direct elim
+          DialogChoices(
+              title = strings.tournamentsCreatePoolSize,
+              selected = "No qualifiers",
+              items = listOf("No qualifiers", "2", "3"),
+              onItemClick = {},
+          ) { Text(it) }
+          DialogChoices(
+              title = strings.tournamentsCreateDirectElimination,
+              selected = "1 / 4",
+              items = listOf("1 / 4", "1 / 2", "Finals"),
+              onItemClick = {},
+          ) { Text(it) }
         }
       },
   )
@@ -101,7 +124,7 @@ private fun DialogPill(
       onClick = onClick,
       modifier = modifier.sizeIn(minWidth = 40.dp, minHeight = 40.dp),
       shape = CircleShape,
-      contentPadding = PaddingValues(),
+      contentPadding = PaddingValues(horizontal = 8.dp),
       border = BorderStroke(2.dp, Green200),
       colors = colors,
   ) {
@@ -113,20 +136,73 @@ private fun DialogPill(
   }
 }
 
+/** The unique key for the title of a [DialogChoices] component. */
+private const val KeyTitle = "title"
+
 /**
+ * A composable which displays a list of available choices, which may be selected depending on the
+ * preferences of the user.
  *
+ * @param T the type of the items to display.
+ * @param title the title for the section.
+ * @param selected the currently selected item.
+ * @param items the list of all the available items.
+ * @param onItemClick a callback which is called when an item is pressed.
+ * @param modifier the [Modifier] for this composable.
+ * @param key the (optional) key for items of type [T].
+ * @param itemContent the body of an item to display.
+ */
+@Composable
+private fun <T> DialogChoices(
+    title: String,
+    selected: T?,
+    items: List<T>,
+    onItemClick: (T) -> Unit,
+    modifier: Modifier = Modifier,
+    key: ((T) -> Any)? = null,
+    itemContent: @Composable (T) -> Unit,
+) {
+  LazyRow(
+      modifier = modifier,
+      horizontalArrangement = spacedBy(8.dp),
+      verticalAlignment = CenterVertically,
+  ) {
+    item(key = KeyTitle) {
+      Text(
+          text = title,
+          style = MaterialTheme.typography.subtitle1,
+          color = PawniesColors.Green500,
+          modifier = Modifier.width(116.dp),
+          maxLines = 1,
+          overflow = Ellipsis,
+      )
+    }
+    items(items = items, key = key) {
+      DialogPill(
+          selected = it == selected,
+          onClick = { onItemClick(it) },
+      ) { itemContent(it) }
+    }
+  }
+}
+
+/**
+ * Some [ButtonColors] which will be applied to a [DialogPill].
+ *
+ * @param selected a [State] which keeps track of whether the pill is selected or not.
  */
 private class DialogPillColors(selected: State<Boolean>) : ButtonColors {
 
+  /** True iff the pill is currently selected. */
   private val selected by selected
 
   @Composable
   override fun backgroundColor(
       enabled: Boolean,
-  ): State<Color> = animateColorAsState(if (selected) Green100 else Green100.copy(alpha = 0f))
+  ) = animateColorAsState(if (selected) Green100 else Green100.copy(alpha = 0f))
 
   @Composable
   override fun contentColor(
       enabled: Boolean,
-  ): State<Color> = animateColorAsState(if (selected) Beige050 else Green200)
+  ) = animateColorAsState(if (selected) Beige050 else Green200)
 }
