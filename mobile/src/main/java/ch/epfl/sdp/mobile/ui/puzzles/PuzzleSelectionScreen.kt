d58b4c103883b86b45e1fad975d3d20853b8373d
matt989253 <matthieu.burguburu@gmail.com>
2022-05-10 14:01:57 +0200

    Explicit PuzzleListInfo parameter

--- mobile/src/main/java/ch/epfl/sdp/mobile/ui/puzzles/PuzzleSelectionScreen.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/ui/puzzles/PuzzleSelectionScreen.kt
@@ -3,7 +3,10 @@ package ch.epfl.sdp.mobile.ui.puzzles
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -68,7 +71,7 @@ fun PuzzleSelectionScreen(
         modifier =
             Modifier.semantics { contentDescription = strings.puzzleListContentDescription }) {
       items(state.puzzles, key) { puzzle ->
-        PuzzleListInfo(puzzle, { state.onPuzzleItemClick(puzzle) })
+        PuzzleListInfo(puzzle, onClick = { state.onPuzzleItemClick(puzzle) })
       }
     }
   }
