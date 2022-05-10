be0d4d64c2a8be92dcf4249a31b265e89d4e7411
Alexandre Piveteau <alexandre@piveteau.email>
2022-05-10 15:40:35 +0200

    Add `Transaction` support to `FirestoreStore` and `FakeStore` (#325)
    * Add `Transaction` support to `FirestoreStore` and `FakeStore`
    * Fix `Transaction` signature and add `FirestoreTransactionTest`
    * Add an additional test to `FirestoreTransactionTest`
    * Add a test for `FakeStore` transaction support

---
+++ mobile/src/main/java/ch/epfl/sdp/mobile/infrastructure/persistence/store/firestore/FirestoreTransaction.kt
@@ -0,0 +1,48 @@
+package ch.epfl.sdp.mobile.infrastructure.persistence.store.firestore
+
+import ch.epfl.sdp.mobile.infrastructure.persistence.store.DocumentEditScope
+import ch.epfl.sdp.mobile.infrastructure.persistence.store.DocumentSnapshot
+import ch.epfl.sdp.mobile.infrastructure.persistence.store.RecordingDocumentEditScope
+import ch.epfl.sdp.mobile.infrastructure.persistence.store.Transaction
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.Transaction as ActualTransaction
+import kotlin.reflect.KClass
+
+class FirestoreTransaction(
+    private val actual: ActualTransaction,
+) : Transaction<FirestoreDocumentReference> {
+
+  override fun set(
+      reference: FirestoreDocumentReference,
+      scope: DocumentEditScope.() -> Unit,
+  ) {
+    val document = RecordingDocumentEditScope().also(scope).mutations.toFirestoreDocument()
+    actual.set(reference.actual, document)
+  }
+
+  override fun <T : Any> set(
+      reference: FirestoreDocumentReference,
+      value: T,
+      valueClass: KClass<T>,
+  ) {
+    actual.set(reference.actual, value)
+  }
+
+  override fun update(
+      reference: FirestoreDocumentReference,
+      scope: DocumentEditScope.() -> Unit,
+  ) {
+    val document = RecordingDocumentEditScope().also(scope).mutations.toFirestoreDocument()
+    actual.set(reference.actual, document, SetOptions.merge())
+  }
+
+  override fun delete(
+      reference: FirestoreDocumentReference,
+  ) {
+    actual.delete(reference.actual)
+  }
+
+  override fun getSnapshot(
+      reference: FirestoreDocumentReference,
+  ): DocumentSnapshot = FirestoreDocumentSnapshot(actual.get(reference.actual))
+}
