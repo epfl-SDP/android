883ff6c0e54ba87fed5cbfc953a749ae22ad6b0d
Fouad-sys <61212919+Fouad-sys@users.noreply.github.com>
2022-05-12 17:22:30 +0200

    Display registered tournaments in Contest Screen (#332)
    * Add Tournament interface and TournamentAdapter, implement getTournaments function and display all registered tournaments in state
    
    * Format code
    
    * Add tournamentFacade in CompositionLocals and to all other tests and add a StatefulTournamentScreen test
    
    * Add test
    
    * Add navigation to tournaments
    
    * Fix tournaments not showing
    
    * Add test and remove join badge in Contest Screen
    
    * Refactor StatefulContestsScreen Actions
    
    Co-authored-by: Alexandre Piveteau <alexandre@piveteau.email>

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/application/TournamentFacadeTest.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/application/TournamentFacadeTest.kt
@@ -7,6 +7,7 @@ import ch.epfl.sdp.mobile.application.tournaments.TournamentFacade
 import ch.epfl.sdp.mobile.application.tournaments.TournamentReference
 import ch.epfl.sdp.mobile.infrastructure.persistence.store.asFlow
 import ch.epfl.sdp.mobile.infrastructure.persistence.store.get
+import ch.epfl.sdp.mobile.test.infrastructure.persistence.auth.buildAuth
 import ch.epfl.sdp.mobile.test.infrastructure.persistence.auth.emptyAuth
 import ch.epfl.sdp.mobile.test.infrastructure.persistence.store.buildStore
 import ch.epfl.sdp.mobile.test.infrastructure.persistence.store.document
@@ -19,6 +20,27 @@ import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 class TournamentFacadeTest {
+
+  @Test
+  fun given_tournamentFacade_when_tournamentsExist_then_getTournamentsReturnsThemAll() {
+    runTest {
+      val auth = buildAuth { user("a@hotmail.com", "password", "1") }
+      val store = buildStore {
+        collection("tournaments") {
+          document("id1", TournamentDocument("id1", "1", "Tournament 1"))
+        }
+      }
+      val tournamentFacade = TournamentFacade(auth, store)
+      val authenticationFacade = AuthenticationFacade(auth, store)
+      val user = authenticationFacade.currentUser.filterIsInstance<AuthenticatedUser>().first()
+
+      val tournaments = tournamentFacade.tournaments(user).first()
+      assertThat(tournaments[0].name).isEqualTo("Tournament 1")
+      assertThat(tournaments[0].reference).isEqualTo(TournamentReference("id1"))
+      assertThat(tournaments[0].isAdmin).isFalse()
+      assertThat(tournaments[0].isParticipant).isFalse()
+    }
+  }
 
   @Test
   fun given_tournamentReference_when_joining_then_addsUserIdentifierInDocument() = runTest {
