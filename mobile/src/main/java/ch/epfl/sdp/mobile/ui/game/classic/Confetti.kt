998d9d3fb49de754ca933c9fe4a255d1276c2b43
Alexandre Piveteau <alexandre@piveteau.email>
2022-05-14 20:36:45 +0200

    Remove a `@Preview`

--- mobile/src/main/java/ch/epfl/sdp/mobile/ui/game/classic/Confetti.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/ui/game/classic/Confetti.kt
@@ -3,12 +3,7 @@
 package ch.epfl.sdp.mobile.ui.game.classic
 
 import androidx.compose.animation.core.*
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawWithContent
@@ -17,7 +12,6 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.ContentDrawScope
 import androidx.compose.ui.graphics.drawscope.withTransform
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.DpOffset.Companion.Zero
 import androidx.compose.ui.unit.dp
@@ -248,21 +242,3 @@ private val Colors =
         Color(0xFFffa62d),
         Color(0xFFff36ff),
     )
-
-@Preview
-@Composable
-private fun ConfettiPreview() {
-  val confetti = rememberConfettiState()
-  val scope = rememberCoroutineScope()
-  Box(Modifier.fillMaxSize(), Alignment.Center) {
-    Button(
-        onClick = {
-          scope.launch {
-            // confetti.spawn() // Spawn a bunch of confetti.
-            confetti.party(2000L) // Spawn 1 confetti per frame for 2000 ms.
-          }
-        },
-        modifier = Modifier.confetti(confetti),
-    ) { Text("Confetti party !!") }
-  }
-}
