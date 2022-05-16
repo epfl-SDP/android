09b2cd8f1c27332916a0983f1afc0930aa940520
Alexandre Piveteau <alexandre@piveteau.email>
2022-05-16 18:05:02 +0200

    Fix `DataStoreFactory` implementation

--- mobile/src/main/java/ch/epfl/sdp/mobile/infrastructure/persistence/datastore/androidx/AndroidXDataStoreFactory.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/infrastructure/persistence/datastore/androidx/AndroidXDataStoreFactory.kt
@@ -10,19 +10,15 @@ import ch.epfl.sdp.mobile.infrastructure.persistence.datastore.Preferences
 /** The datastore file name. */
 private const val DefaultDatastoreFileName = "preferences"
 
+/** The actual underlying preferences data store. */
+private val Context.dataStore by preferencesDataStore(DefaultDatastoreFileName)
+
 /**
  * An implementation of a [DataStoreFactory] which is based on the AndroidX library.
  *
  * @param context the underlying [Context].
- * @param name the name of the preferences file.
  */
-class AndroidXDataStoreFactory(
-    private val context: Context,
-    name: String = DefaultDatastoreFileName,
-) : DataStoreFactory {
-
-  /** The actual underlying preferences data store. */
-  private val Context.dataStore by preferencesDataStore(name)
+class AndroidXDataStoreFactory(private val context: Context) : DataStoreFactory {
 
   override fun createPreferencesDataStore(): Pair<DataStore<Preferences>, KeyFactory> =
       AndroidXPreferencesDataStore(context.dataStore) to AndroidXKeyFactory
