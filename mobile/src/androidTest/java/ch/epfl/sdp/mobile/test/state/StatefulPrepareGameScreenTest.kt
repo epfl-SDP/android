7ab8457cad27ebcd46c550c39136b156761a2a27
Alexandre Piveteau <alexandre@piveteau.email>
2022-05-16 23:20:38 +0200

    Refactor `StatefulPrepareGameScreenTest`

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/state/StatefulPrepareGameScreenTest.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/state/StatefulPrepareGameScreenTest.kt
@@ -17,7 +17,6 @@ import ch.epfl.sdp.mobile.test.infrastructure.assets.fake.emptyAssets
 import ch.epfl.sdp.mobile.test.infrastructure.persistence.auth.emptyAuth
 import ch.epfl.sdp.mobile.test.infrastructure.persistence.store.buildStore
 import ch.epfl.sdp.mobile.test.infrastructure.persistence.store.document
-import ch.epfl.sdp.mobile.test.infrastructure.persistence.store.emptyStore
 import ch.epfl.sdp.mobile.test.infrastructure.speech.FailingSpeechRecognizerFactory
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.channels.Channel
@@ -34,23 +33,14 @@ class StatefulPrepareGameScreenTest {
 
   @Test
   fun defaultScreen_isDisplayed() = runTest {
-    val auth = emptyAuth()
-    val store = emptyStore()
-    val assets = emptyAssets()
-    val facade = AuthenticationFacade(auth, store)
-    val social = SocialFacade(auth, store)
-    val chess = ChessFacade(auth, store, assets)
-    val speech = SpeechFacade(FailingSpeechRecognizerFactory)
-    val tournament = TournamentFacade(auth, store)
-
-    facade.signUpWithEmail("email@epfl.ch", "name", "password")
-    val user = facade.currentUser.filterIsInstance<AuthenticatedUser>().first()
-    val strings =
-        rule.setContentWithLocalizedStrings {
-          ProvideFacades(facade, social, chess, speech, tournament) {
-            StatefulPrepareGameScreen(
-                user = user, navigateToGame = {}, cancelClick = {}, opponentId = "")
-          }
+    val (_, _, strings) =
+        rule.setContentWithTestEnvironment {
+          StatefulPrepareGameScreen(
+              user = user,
+              navigateToGame = {},
+              cancelClick = {},
+              opponentId = "",
+          )
         }
 
     rule.onNodeWithText(strings.prepareGameWhiteColor).assertExists()
@@ -59,23 +49,14 @@ class StatefulPrepareGameScreenTest {
 
   @Test
   fun switchColorToBlack_works() = runTest {
-    val auth = emptyAuth()
-    val store = emptyStore()
-    val assets = emptyAssets()
-    val facade = AuthenticationFacade(auth, store)
-    val social = SocialFacade(auth, store)
-    val chess = ChessFacade(auth, store, assets)
-    val speech = SpeechFacade(FailingSpeechRecognizerFactory)
-    val tournament = TournamentFacade(auth, store)
-
-    facade.signUpWithEmail("email@epfl.ch", "name", "password")
-    val user = facade.currentUser.filterIsInstance<AuthenticatedUser>().first()
-    val strings =
-        rule.setContentWithLocalizedStrings {
-          ProvideFacades(facade, social, chess, speech, tournament) {
-            StatefulPrepareGameScreen(
-                user = user, navigateToGame = {}, cancelClick = {}, opponentId = "")
-          }
+    val (_, _, strings) =
+        rule.setContentWithTestEnvironment {
+          StatefulPrepareGameScreen(
+              user = user,
+              navigateToGame = {},
+              cancelClick = {},
+              opponentId = "",
+          )
         }
 
     rule.onNodeWithText(strings.prepareGameChooseColor).assertExists()
@@ -84,23 +65,14 @@ class StatefulPrepareGameScreenTest {
 
   @Test
   fun switchColorBackToWhite_works() = runTest {
-    val auth = emptyAuth()
-    val store = emptyStore()
-    val assets = emptyAssets()
-    val facade = AuthenticationFacade(auth, store)
-    val social = SocialFacade(auth, store)
-    val chess = ChessFacade(auth, store, assets)
-    val speech = SpeechFacade(FailingSpeechRecognizerFactory)
-    val tournament = TournamentFacade(auth, store)
-
-    facade.signUpWithEmail("email@epfl.ch", "name", "password")
-    val user = facade.currentUser.filterIsInstance<AuthenticatedUser>().first()
-    val strings =
-        rule.setContentWithLocalizedStrings {
-          ProvideFacades(facade, social, chess, speech, tournament) {
-            StatefulPrepareGameScreen(
-                user = user, navigateToGame = {}, cancelClick = {}, opponentId = "")
-          }
+    val (_, _, strings) =
+        rule.setContentWithTestEnvironment {
+          StatefulPrepareGameScreen(
+              user = user,
+              navigateToGame = {},
+              cancelClick = {},
+              opponentId = "",
+          )
         }
 
     rule.onNodeWithText(strings.prepareGameChooseColor).assertExists()
