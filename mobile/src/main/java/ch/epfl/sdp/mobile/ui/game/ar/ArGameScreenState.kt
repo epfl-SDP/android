f1a7528e42e7eff069401a933d89a83b9ebf0d0a
ChauYingKot <chau.kot@epfl.ch>
2022-05-17 18:05:04 +0200

    Add doc on `ArGameScrrenState`

--- mobile/src/main/java/ch/epfl/sdp/mobile/ui/game/ar/ArGameScreenState.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/ui/game/ar/ArGameScreenState.kt
@@ -3,14 +3,27 @@ package ch.epfl.sdp.mobile.ui.game.ar
 import androidx.compose.runtime.Stable
 import ch.epfl.sdp.mobile.ui.game.ChessBoardState
 
+/**
+ * A interface which represent tht state of a [ArChessBoardScreen]
+ *
+ * @param Piece the type of the [ChessBoardState.Piece].
+ */
 @Stable
 interface ArGameScreenState<Piece : ChessBoardState.Piece> {
 
+  // Scene that contains all information about the AR
   val chessScene: ChessScene<Piece>
 
+  // A map of pieces and their position that represent the current state of the game
   val pieces: Map<ChessBoardState.Position, Piece>
 
+  /**
+   * Ask the [chessScene] to load the scene
+   *
+   * @param boardScale scale factor of the whole scene
+   */
   fun onLoad(boardScale: Float)
 
+  /** Update the display AR Scene */
   fun update()
 }
