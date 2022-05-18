c60682ecef14d4480dba5e1dfc83570669355c97
Juri <Chau-ying-kot@hotmail.com>
2022-05-18 21:52:58 +0200

    Simplify Ar delegation

--- mobile/src/main/java/ch/epfl/sdp/mobile/state/StatefullArScreen.kt
+++ mobile/src/main/java/ch/epfl/sdp/mobile/state/StatefullArScreen.kt
@@ -2,8 +2,10 @@ package ch.epfl.sdp.mobile.state
 
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import ch.epfl.sdp.mobile.state.game.ActualArScreenState
+import ch.epfl.sdp.mobile.state.game.delegating.DelegatingArState
+import ch.epfl.sdp.mobile.state.game.delegating.DelegatingChessBoardState
 import ch.epfl.sdp.mobile.ui.game.ar.ArChessBoardScreen
+import ch.epfl.sdp.mobile.ui.game.ar.ChessScene
 
 /**
  * A composable that make [ArChessBoardScreen] stateful
@@ -21,7 +23,9 @@ fun StatefulArScreen(
   val scope = rememberCoroutineScope()
   val match = remember(chessFacade, id) { chessFacade.match(id) }
 
-  val state = remember(match, scope) { ActualArScreenState(scope, match) }
+  val scene = ChessScene<DelegatingChessBoardState.Piece>(scope)
+
+  val state = remember(match, scope) { DelegatingArState(match, scene, scope) }
 
   ArChessBoardScreen(state, modifier)
 }
