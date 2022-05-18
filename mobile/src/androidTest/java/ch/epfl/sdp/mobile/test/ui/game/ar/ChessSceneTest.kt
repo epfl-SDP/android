801de48ec176c013663f632559258fe956d41781
ChauYingKot <chau.kot@epfl.ch>
2022-05-18 13:45:17 +0200

    Add and update tests on `ChessScene`

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/ui/game/ar/ChessSceneTest.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/ui/game/ar/ChessSceneTest.kt
@@ -1,18 +1,27 @@
 package ch.epfl.sdp.mobile.test.ui.game.ar
 
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import ch.epfl.sdp.mobile.state.HomeActivity
 import ch.epfl.sdp.mobile.state.game.delegating.DelegatingChessBoardState
+import ch.epfl.sdp.mobile.test.assertThrows
 import ch.epfl.sdp.mobile.ui.game.ar.ChessScene
 import dev.romainguy.kotlin.math.Float3
+import java.lang.IllegalStateException
 import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotEquals
+import org.junit.Rule
 import org.junit.Test
 
 class ChessSceneTest {
 
+  @get:Rule val rule = createAndroidComposeRule<HomeActivity>()
+
   @Test
   fun given_emptyState_when_initChessScene_then_rootIsNotNull() {
     val chessScene = ChessScene<DelegatingChessBoardState.Piece>(TestScope())
+    chessScene.context = rule.activity.applicationContext
 
     assertNotEquals(null, chessScene.boardNode)
   }
@@ -20,8 +29,16 @@ class ChessSceneTest {
   @Test
   fun given_chessScene_when_scaled_then_rootHasCorrectScaleVector() {
     val chessScene = ChessScene<DelegatingChessBoardState.Piece>(TestScope())
+    chessScene.context = rule.activity.applicationContext
 
     chessScene.scale(4f)
     assertEquals(Float3(4f), chessScene.boardNode.scale)
+  }
+
+  @Test
+  fun given_chessSceneWithoutContext_when_callUpdate_then_throwIllegalStateException() = runTest {
+    val chessScene = ChessScene<DelegatingChessBoardState.Piece>(this)
+
+    assertThrows<IllegalStateException> { chessScene.loadBoard(emptyMap()) }
   }
 }
