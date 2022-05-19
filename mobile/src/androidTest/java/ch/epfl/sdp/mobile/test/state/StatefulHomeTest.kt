81f2edb2c68e1ec7c1c7f8b824026c14e6c3f71d
ChauYingKot <chau.kot@epfl.ch>
2022-05-19 15:53:57 +0200

    Fix formatting

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/state/StatefulHomeTest.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/state/StatefulHomeTest.kt
@@ -415,7 +415,7 @@ class StatefulHomeTest {
     rule.onNodeWithText("Player 2").assertExists()
   }
 
-  //@Test
+  // @Test
   fun given_aOnGoingGame_when_clickOnArButton_then_displayArScreen() = runTest {
     val auth = buildAuth { user("email@example.org", "password", "1") }
     val store = buildStore {
@@ -559,37 +559,38 @@ class StatefulHomeTest {
   @Test
   fun given_contestScreen_when_tournamentIsClicked_then_itsTournamentDetailsScreenIsDisplayed() =
       runTest {
-    val auth = buildAuth { user("email@example.org", "password", "1") }
-    val store = buildStore {
-      collection(TournamentDocument.Collection) {
-        document("id1", TournamentDocument("tid1", "1", "Tournament 1"))
-      }
-    }
-    val assets = emptyAssets()
-
-    val authFacade = AuthenticationFacade(auth, store)
-    val chessFacade = ChessFacade(auth, store, assets)
-    val socialFacade = SocialFacade(auth, store)
-    val speechFacade = SpeechFacade(UnknownCommandSpeechRecognizerFactory)
-    val tournamentFacade = TournamentFacade(auth, store)
-
-    val user = authFacade.currentUser.filterIsInstance<AuthenticatedUser>().first()
-
-    val strings =
-        rule.setContentWithLocalizedStrings {
-          val controller = rememberNavController()
-          ProvideFacades(authFacade, socialFacade, chessFacade, speechFacade, tournamentFacade) {
-            StatefulHome(
-                user = user,
-                controller = controller,
-            )
+        val auth = buildAuth { user("email@example.org", "password", "1") }
+        val store = buildStore {
+          collection(TournamentDocument.Collection) {
+            document("id1", TournamentDocument("tid1", "1", "Tournament 1"))
           }
         }
+        val assets = emptyAssets()
 
-    rule.onNodeWithText(strings.sectionContests).performClick()
-    rule.onNodeWithText("Tournament 1").assertExists()
-    rule.onNodeWithText("Tournament 1").performClick()
-    rule.onNodeWithText(strings.tournamentsBadgeJoin).assertIsDisplayed()
-    rule.onNodeWithText("Tournament 1", ignoreCase = true).assertIsDisplayed()
-  }
+        val authFacade = AuthenticationFacade(auth, store)
+        val chessFacade = ChessFacade(auth, store, assets)
+        val socialFacade = SocialFacade(auth, store)
+        val speechFacade = SpeechFacade(UnknownCommandSpeechRecognizerFactory)
+        val tournamentFacade = TournamentFacade(auth, store)
+
+        val user = authFacade.currentUser.filterIsInstance<AuthenticatedUser>().first()
+
+        val strings =
+            rule.setContentWithLocalizedStrings {
+              val controller = rememberNavController()
+              ProvideFacades(
+                  authFacade, socialFacade, chessFacade, speechFacade, tournamentFacade) {
+                StatefulHome(
+                    user = user,
+                    controller = controller,
+                )
+              }
+            }
+
+        rule.onNodeWithText(strings.sectionContests).performClick()
+        rule.onNodeWithText("Tournament 1").assertExists()
+        rule.onNodeWithText("Tournament 1").performClick()
+        rule.onNodeWithText(strings.tournamentsBadgeJoin).assertIsDisplayed()
+        rule.onNodeWithText("Tournament 1", ignoreCase = true).assertIsDisplayed()
+      }
 }
