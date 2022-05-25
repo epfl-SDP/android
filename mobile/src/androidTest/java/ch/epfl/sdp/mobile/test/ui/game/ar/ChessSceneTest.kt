884ec5e8d5282aaa75dbc20d732364ad8fb0651c
ChauYingKot <chau.kot@epfl.ch>
2022-05-25 13:38:17 +0200

    Use `ActivityScenarioRule` inside `ChessSceneTest`

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/ui/game/ar/ChessSceneTest.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/ui/game/ar/ChessSceneTest.kt
@@ -1,75 +1,90 @@
 package ch.epfl.sdp.mobile.test.ui.game.ar
 
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.activity.ComponentActivity
 import androidx.lifecycle.lifecycleScope
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import ch.epfl.sdp.mobile.application.chess.engine.Color.*
 import ch.epfl.sdp.mobile.application.chess.engine.Game
 import ch.epfl.sdp.mobile.application.chess.engine.Piece as EnginePiece
 import ch.epfl.sdp.mobile.application.chess.engine.PieceIdentifier
 import ch.epfl.sdp.mobile.application.chess.engine.Rank
-import ch.epfl.sdp.mobile.state.HomeActivity
 import ch.epfl.sdp.mobile.state.game.delegating.DelegatingChessBoardState.Companion.toPosition
 import ch.epfl.sdp.mobile.state.game.delegating.DelegatingChessBoardState.Piece
 import ch.epfl.sdp.mobile.ui.game.ChessBoardState.*
 import ch.epfl.sdp.mobile.ui.game.ar.ChessScene
 import com.google.common.truth.Truth.assertThat
 import io.github.sceneview.math.Scale
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.yield
 import org.junit.Rule
 import org.junit.Test
 
-// Increase the timeout to allow the CI to have enough time to load the 3D Model
-const val Timeout = 5000L
-
 class ChessSceneTest {
 
-  @get:Rule val rule = createAndroidComposeRule<HomeActivity>()
+  @get:Rule val rule = ActivityScenarioRule(ComponentActivity::class.java)
 
   private val simpleBoard =
       Game.create().board.associate { (pos, piece) -> pos.toPosition() to Piece(piece) }
 
   @Test
   fun given_emptyState_when_initChessScene_then_has32ChildrenNodes() = runTest {
-    val context = rule.activity.applicationContext
+    rule.scenario.onActivity {
+      val chessScene = ChessScene(it.applicationContext, it.lifecycleScope, simpleBoard)
 
-    val chessScene = ChessScene(context, rule.activity.lifecycleScope, simpleBoard)
-
-    rule.waitUntil(Timeout) { chessScene.boardNode.children.size == simpleBoard.size }
-
-    assertThat(chessScene.boardNode.children.size).isEqualTo(simpleBoard.size)
+      launch {
+        yieldWhile { chessScene.boardNode.children.size != simpleBoard.size }
+        assertThat(chessScene.boardNode.children.size).isEqualTo(simpleBoard.size)
+      }
+    }
   }
 
   @Test
   fun given_chessScene_when_scaled_then_rootHasCorrectScaleVector() = runTest {
-    val context = rule.activity.applicationContext
+    rule.scenario.onActivity {
+      val chessScene = ChessScene(it.applicationContext, it.lifecycleScope, simpleBoard)
 
-    val chessScene = ChessScene(context, rule.activity.lifecycleScope, simpleBoard)
+      launch {
+        yieldWhile { chessScene.boardNode.children.size != simpleBoard.size }
 
-    rule.waitUntil(Timeout) { chessScene.boardNode.children.size == simpleBoard.size }
-
-    chessScene.scale(4f)
-    assertThat(chessScene.boardNode.scale).isEqualTo(Scale(4f))
+        chessScene.scale(4f)
+        assertThat(chessScene.boardNode.scale).isEqualTo(Scale(4f))
+      }
+    }
   }
 
   @Test
-  fun given_chessScene_whenUpdated_then_haveCorrectNumberOfChildren() {
-    val context = rule.activity.applicationContext
+  fun given_chessScene_whenUpdated_then_haveCorrectNumberOfChildren() = runTest {
+    rule.scenario.onActivity {
+      val chessScene = ChessScene(it.applicationContext, it.lifecycleScope, simpleBoard)
 
-    val chessScene = ChessScene(context, rule.activity.lifecycleScope, simpleBoard)
-    rule.waitUntil(Timeout) { chessScene.boardNode.children.size == simpleBoard.size }
+      val iterator = simpleBoard.entries.iterator()
+      val oldMovePiece = iterator.next()
+      val newMovePiece = oldMovePiece.apply { (Position(4, 4) to value) }.toPair()
 
-    val iterator = simpleBoard.entries.iterator()
-    val oldMovePiece = iterator.next()
-    val newMovePiece = oldMovePiece.apply { (Position(4, 4) to value) }.toPair()
+      val newPiece = (Position(4, 5) to Piece(EnginePiece(White, Rank.Pawn, PieceIdentifier(40))))
+      val newBoard = mapOf(newMovePiece, newPiece)
 
-    val newPiece = (Position(4, 5) to Piece(EnginePiece(White, Rank.Pawn, PieceIdentifier(40))))
+      launch {
+        yieldWhile { chessScene.boardNode.children.size != simpleBoard.size }
 
-    val newBoard = mapOf(newMovePiece, newPiece)
+        chessScene.update(newBoard)
+      }
 
-    chessScene.update(newBoard)
+      launch {
+        yieldWhile { chessScene.boardNode.children.size != newBoard.size }
 
-    rule.waitUntil(Timeout) { chessScene.boardNode.children.size == newBoard.size }
-
-    assertThat(chessScene.boardNode.children.size).isEqualTo(newBoard.size)
+        assertThat(chessScene.boardNode.children.size).isEqualTo(newBoard.size)
+      }
+    }
   }
+}
+
+/**
+ * This function yield until the [predicate] is not satisfied.
+ *
+ * @param predicate If this function return true yield, otherwise the function end
+ */
+private suspend fun yieldWhile(predicate: () -> Boolean) {
+  while (predicate()) yield()
 }
