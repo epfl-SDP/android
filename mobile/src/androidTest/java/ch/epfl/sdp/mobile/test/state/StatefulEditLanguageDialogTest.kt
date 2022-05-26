a7a980a8a0589d381008b9e72b45c89bf4679649
Lars Barmettler <barmettler.lars@gmail.com>
2022-05-26 19:33:08 +0200

    hope to get enougth test coverage

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/state/StatefulEditLanguageDialogTest.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/state/StatefulEditLanguageDialogTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import ch.epfl.sdp.mobile.state.Navigation
 import ch.epfl.sdp.mobile.ui.i18n.English
 import ch.epfl.sdp.mobile.ui.i18n.German
+import ch.epfl.sdp.mobile.ui.i18n.SwissGerman
 import ch.epfl.sdp.mobile.ui.setting.Languages
 import kotlinx.coroutines.test.runTest
 import org.junit.Rule
@@ -19,10 +20,23 @@ class StatefulEditLanguageDialogTest {
     val (_, _, strings) = rule.setContentWithTestEnvironment { Navigation() }
 
     rule.onNodeWithText(strings.sectionSettings).performClick()
-    rule.onNodeWithText(English.settingsEditLanguage).assertExists()
-    rule.onNodeWithText(English.settingsEditLanguage).performClick()
+    rule.onNodeWithText(strings.settingsEditLanguage).performClick()
     rule.onNodeWithText(Languages.keys.elementAt(2)).performClick()
     rule.onNodeWithText(strings.settingEditSave).performClick()
     rule.onNodeWithText(German.settingsEditLanguage).assertIsDisplayed()
+    rule.onNodeWithText(German.settingsEditLanguage).performClick()
+    rule.onNodeWithText(Languages.keys.elementAt(3)).performClick()
+    rule.onNodeWithText(German.settingEditSave).performClick()
+    rule.onNodeWithText(SwissGerman.settingsEditLanguage).assertIsDisplayed()
+  }
+
+  @Test
+  fun given_userIsLoggedIn_when_openedChangeLanguageDialog_then_clickCancleButton() = runTest {
+    val (_, _, strings) = rule.setContentWithTestEnvironment { Navigation() }
+
+    rule.onNodeWithText(strings.sectionSettings).performClick()
+    rule.onNodeWithText(strings.settingsEditLanguage).performClick()
+    rule.onNodeWithText(strings.settingEditCancel).performClick()
+    rule.onNodeWithText(English.settingsEditLanguage).assertIsDisplayed()
   }
 }
