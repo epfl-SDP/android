646e2729038315e244b2af53e11271a2b099853d
Alexandre Piveteau <alexandre@piveteau.email>
2022-05-29 23:52:09 +0200

    Refactor some UI tests by introducing a bunch of robots (#431)
    * Add `hasLocalizedText` to `AbstractRobot`
    
    * Add a bunch of tests
    
    * Migrate more stateful components tests
    
    * Refactor some additional tests
    
    * Apply Ktfmt
    
    * Fix `ColorChoiceTabBar` accessibility, add a test and use more robots
    
    * Change `setContentWithTestEnvironment` to `setContentWithAuthenticatedTestEnvironment`
    
    * Apply some feedback

--- mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/ui/game/ChessBoardRobot.kt
+++ mobile/src/androidTest/java/ch/epfl/sdp/mobile/test/ui/game/ChessBoardRobot.kt
@@ -138,6 +138,16 @@ class ChessBoardRobot(
     }
   }
 
+  /** Asserts that this robot is currently displayed. */
+  fun assertIsDisplayed() {
+    onChessBoard().assertExists()
+  }
+
+  /** Asserts that this robot is currently not displayed. */
+  fun assertIsNotDisplayed() {
+    onChessBoard().assertDoesNotExist()
+  }
+
   /**
    * Asserts that a piece with the given [color] and [rank] is present at the given position. This
    * will essentially check that the center of the piece is present in a specific cell.
