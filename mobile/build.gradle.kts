183cacfe76b261b825a74d42918dbe532a5d3e4e
Alexandre Piveteau <alexandre@piveteau.email>
2022-06-09 19:21:02 +0200

    Bump the version number

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -16,7 +16,7 @@ android {
     minSdk = 24
     targetSdk = 31
     versionCode = 1
-    versionName = "0.1.0"
+    versionName = "0.1.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
